71ce0b24f9d052b00c9220ac12a9cac6d5a34166
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-27 21:49:08 +0100

    Add a script to verify the line lengths of text items

---
+++ transliteration-scripts/check-line-widths.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import re
+import pokemon
+import sys
+
+for filename in sys.argv[1:]:
+    with open(filename, "r", encoding="utf-8") as f:
+        for line_number, line in enumerate(f):
+            md = pokemon.TEXT_RE.match(line)
+
+            if md is None:
+                continue
+
+            text = md.group(1)
+
+            length = pokemon.word_length(text)
+
+            if length > pokemon.MAX_LINE_LENGTH:
+                print(f"{filename}:{line_number + 1}: ({length}) {text}")
