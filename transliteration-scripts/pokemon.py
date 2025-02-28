78481840e8016d84e8ba4beeb42bb14f504a1c87
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-28 10:43:33 +0100

    transliteration-scripts: Fix the 𐑩𐑛 ligature

--- transliteration-scripts/pokemon.py
+++ transliteration-scripts/pokemon.py
@@ -2,7 +2,7 @@ import re
 
 TEXT_RE = re.compile(r'^[\t ]*(?:text|next|page) "([^"]*)" *$')
 MAX_LINE_LENGTH = 18
-SINGLE_CHARACTER_RE = re.compile(r'𐑦𐑑|𐑦𐑓|𐑦𐑕|𐑦𐑖|𐑦𐑟|𐑦𐑯|𐑩𐑯|𐑾𐑯|𐑦𐑙|𐑩𐑑|𐑯𐑑|.')
+SINGLE_CHARACTER_RE = re.compile(r'𐑦𐑑|𐑦𐑓|𐑦𐑕|𐑦𐑖|𐑦𐑟|𐑦𐑯|𐑩𐑯|𐑾𐑯|𐑦𐑙|𐑩𐑛|𐑯𐑑|.')
 
 def word_length(word):
     # Count the known ligatures as a single character
