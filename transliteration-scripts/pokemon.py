490af49b5562630780ecb5e5c34a75b70784ecbe
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-28 22:22:33 +0100

    transliteration-scripts: Add an override length for <RIVAL>

--- transliteration-scripts/pokemon.py
+++ transliteration-scripts/pokemon.py
@@ -20,6 +20,7 @@ REPLACEMENT_LENGTHS = {
 
     "#": 4, # 𐑐𐑴𐑒𐑱
     "<PLAYER>": 7 + 1, # maximum player name length + 1 for the namer dot
+    "<RIVAL>": 7 + 1, # same for the rival
 
     # Pokemon names. It looks like the maximum length is 10?
     "<USER>": 10,
