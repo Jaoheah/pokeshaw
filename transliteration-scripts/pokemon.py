f4479347671e98db070e5f2905042ed01cfe12e2
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-28 10:42:55 +0100

    transliteration-scripts: Increase the max line length to 18
    It looks like there are quite a few lines that are 18 characters long in
    the original text.

--- transliteration-scripts/pokemon.py
+++ transliteration-scripts/pokemon.py
@@ -1,7 +1,7 @@
 import re
 
 TEXT_RE = re.compile(r'^[\t ]*(?:text|next|page) "([^"]*)" *$')
-MAX_LINE_LENGTH = 17
+MAX_LINE_LENGTH = 18
 SINGLE_CHARACTER_RE = re.compile(r'𐑦𐑑|𐑦𐑓|𐑦𐑕|𐑦𐑖|𐑦𐑟|𐑦𐑯|𐑩𐑯|𐑾𐑯|𐑦𐑙|𐑩𐑑|𐑯𐑑|.')
 
 def word_length(word):
