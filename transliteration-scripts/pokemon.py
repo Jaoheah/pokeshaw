3917f9b8d6d4c1ca766b17da5d6022cb0634229d
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-02-28 11:10:43 +0100

    transliteration-scripts: Add more replacement lengths for word wrapping

--- transliteration-scripts/pokemon.py
+++ transliteration-scripts/pokemon.py
@@ -2,9 +2,40 @@ import re
 
 TEXT_RE = re.compile(r'^[\t ]*(?:text|next|page) "([^"]*)" *$')
 MAX_LINE_LENGTH = 18
-SINGLE_CHARACTER_RE = re.compile(r'𐑦𐑑|𐑦𐑓|𐑦𐑕|𐑦𐑖|𐑦𐑟|𐑦𐑯|𐑩𐑯|𐑾𐑯|𐑦𐑙|𐑩𐑛|𐑯𐑑|.')
 
+# These sequences end up with a different length when assembled
+REPLACEMENT_LENGTHS = {
+    # Ligatures
+    "𐑦𐑑": 1,
+    "𐑦𐑓": 1,
+    "𐑦𐑕": 1,
+    "𐑦𐑖": 1,
+    "𐑦𐑟": 1,
+    "𐑦𐑯": 1,
+    "𐑩𐑯": 1,
+    "𐑾𐑯": 1,
+    "𐑦𐑙": 1,
+    "𐑩𐑛": 1,
+    "𐑯𐑑": 1,
+
+    "#": 4, # 𐑐𐑴𐑒𐑱
+    "<PLAYER>": 7, # maximum player name length
+
+    # Pokemon names. It looks like the maximum length is 10?
+    "<USER>": 10,
+    "<TARGET>": 10,
+}
+
+_replacements = list(REPLACEMENT_LENGTHS.keys())
+_replacements.sort(key=lambda x: 1000 - len(x))
+
+REPLACEMENT_LENGTHS_RE = re.compile("|".join(map(re.escape, _replacements)) +
+                                    "|.")
 def word_length(word):
-    # Count the known ligatures as a single character
-    return sum(1 for _ in SINGLE_CHARACTER_RE.finditer(word))
+    def match_length(md):
+        try:
+            return REPLACEMENT_LENGTHS[md.group(0)]
+        except KeyError:
+            return 1
 
+    return sum(map(match_length, REPLACEMENT_LENGTHS_RE.finditer(word)))
