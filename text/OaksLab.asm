8162db78a517eec0f16d2a2bccc79f08612e85c0
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-03 18:52:09 +0100

    Transliterate Prof.Oak’s lab text

--- text/OaksLab.asm
+++ text/OaksLab.asm
@@ -1,173 +1,166 @@
 _OaksLabRivalGrampsIsntAroundText::
-	text "<RIVAL>: Yo"
-	line "<PLAYER>! Gramps"
-	cont "isn't around!"
+	text "<RIVAL>: 𐑘𐑴"
+	line "<PLAYER>! ·𐑜𐑮𐑨𐑥𐑐𐑕"
+	cont "𐑦𐑟𐑩𐑯𐑑 𐑼𐑬𐑯𐑛!"
 	done
 
 _OaksLabRivalGoAheadAndChooseText::
-	text "<RIVAL>: Heh, I"
-	line "don't need to be"
-	cont "greedy like you!"
+	text "<RIVAL>: 𐑣𐑧, 𐑲"
+	line "𐑛𐑴𐑯𐑑 𐑯𐑰𐑛 𐑑 𐑚𐑰"
+	cont "𐑜𐑮𐑰𐑛𐑦 𐑤𐑲𐑒 𐑿!"
 
-	para "Go ahead and"
-	line "choose, <PLAYER>!"
+	para "𐑜𐑴 𐑩𐑣𐑧𐑛 𐑯 𐑗𐑵𐑟,"
+	line "<PLAYER>!"
 	done
 
 _OaksLabRivalMyPokemonLooksStrongerText::
-	text "<RIVAL>: My"
-	line "#MON looks a"
-	cont "lot stronger."
+	text "<RIVAL>: 𐑥𐑲"
+	line "#𐑥𐑪𐑯 𐑤𐑫𐑒𐑕 𐑩"
+	cont "𐑤𐑪𐑑 𐑕𐑑𐑮𐑪𐑙𐑜𐑼."
 	done
 
 _OaksLabThoseArePokeBallsText::
-	text "Those are #"
-	line "BALLs. They"
-	cont "contain #MON!"
+	text "𐑞𐑴𐑟 𐑸 # 𐑚𐑷𐑤𐑟."
+	line "𐑞𐑱 𐑒𐑩𐑯𐑑𐑱𐑯 #𐑥𐑪𐑯!"
 	done
 
 _OaksLabYouWantCharmanderText::
-	text "So! You want the"
-	line "fire #MON,"
-	cont "CHARMANDER?"
+	text "𐑕𐑴! 𐑿 𐑢𐑪𐑯𐑑 𐑞 𐑓𐑲𐑼"
+	line "#𐑥𐑪𐑯, 𐑗𐑸𐑥𐑨𐑯𐑛𐑼?"
 	done
 
 _OaksLabYouWantSquirtleText::
-	text "So! You want the"
-	line "water #MON,"
-	cont "SQUIRTLE?"
+	text "𐑕𐑴! 𐑿 𐑢𐑪𐑯𐑑 𐑞 𐑢𐑷𐑑𐑼"
+	line "#𐑥𐑪𐑯, 𐑕𐑒𐑢𐑻𐑑𐑩𐑤?"
 	done
 
 _OaksLabYouWantBulbasaurText::
-	text "So! You want the"
-	line "plant #MON,"
-	cont "BULBASAUR?"
+	text "𐑕𐑴! 𐑿 𐑢𐑪𐑯𐑑 𐑞 𐑐𐑤𐑭𐑯𐑑"
+	line "#𐑥𐑪𐑯, 𐑚𐑳𐑤𐑚𐑩𐑕𐑹?"
 	done
 
 _OaksLabMonEnergeticText::
-	text "This #MON is"
-	line "really energetic!"
+	text "𐑞𐑦𐑕 #𐑥𐑪𐑯 𐑦𐑟"
+	line "𐑮𐑾𐑤𐑦 𐑧𐑯𐑼𐑡𐑧𐑑𐑦𐑒!"
 	prompt
 
 _OaksLabReceivedMonText::
-	text "<PLAYER> received"
-	line "a @"
+	text "<PLAYER> 𐑮𐑦𐑕𐑰𐑝𐑛"
+	line "𐑩 @"
 	text_ram wNameBuffer
 	text "!@"
 	text_end
 
 _OaksLabLastMonText::
-	text "That's PROF.OAK's"
-	line "last #MON!"
+	text "𐑞𐑨𐑑𐑕 ·𐑐𐑮𐑪𐑓.𐑴𐑒𐑕"
+	line "𐑤𐑭𐑕𐑑 #𐑥𐑪𐑯!"
 	done
 
 _OaksLabOak1WhichPokemonDoYouWantText::
-	text "OAK: Now, <PLAYER>,"
-	line "which #MON do"
-	cont "you want?"
+	text "·𐑴𐑒: 𐑯𐑬, <PLAYER>,"
+	line "𐑢𐑦𐑗 #𐑥𐑪𐑯 𐑛𐑵 𐑿"
+	cont "𐑢𐑪𐑯𐑑?"
 	done
 
 _OaksLabOak1YourPokemonCanFightText::
-	text "OAK: If a wild"
-	line "#MON appears,"
-	cont "your #MON can"
-	cont "fight against it!"
+	text "·𐑴𐑒: 𐑦𐑓 𐑩 𐑢𐑲𐑤𐑛"
+	line "#𐑥𐑪𐑯 𐑩𐑐𐑽𐑟, 𐑘𐑹"
+	cont "#𐑥𐑪𐑯 𐑒𐑨𐑯 𐑓𐑲𐑑"
+	cont "𐑩𐑜𐑧𐑯𐑕𐑑 𐑦𐑑!"
 	done
 
 _OaksLabOak1RaiseYourYoungPokemonText::
-	text "OAK: <PLAYER>,"
-	line "raise your young"
-	cont "#MON by making"
-	cont "it fight!"
+	text "·𐑴𐑒: <PLAYER>, 𐑮𐑱𐑟"
+	line "𐑘𐑹 𐑘𐑳𐑙 #𐑥𐑪𐑯 𐑚𐑲"
+	cont "𐑥𐑱𐑒𐑦𐑙 𐑦𐑑 𐑓𐑲𐑑!"
 	done
 
 _OaksLabOak1DeliverParcelText::
-	text "OAK: Oh, <PLAYER>!"
+	text "·𐑴𐑒: 𐑴, <PLAYER>!"
 
-	para "How is my old"
-	line "#MON?"
+	para "𐑣𐑬 𐑦𐑟 𐑥𐑲 𐑴𐑤𐑛"
+	line "#𐑥𐑪𐑯?"
 
-	para "Well, it seems to"
-	line "like you a lot."
+	para "𐑢𐑧𐑤, 𐑦𐑑 𐑕𐑰𐑥𐑟 𐑑 𐑤𐑲𐑒"
+	line "𐑿 𐑩 𐑤𐑪𐑑."
 
-	para "You must be"
-	line "talented as a"
-	cont "#MON trainer!"
+	para "𐑿 𐑥𐑳𐑕𐑑 𐑚𐑰 𐑑𐑨𐑤𐑩𐑯𐑑𐑩𐑛"
+	line "𐑨𐑟 𐑩 #𐑥𐑪𐑯"
+	cont "𐑑𐑮𐑱𐑯𐑼!"
 
-	para "What? You have"
-	line "something for me?"
+	para "𐑢𐑪𐑑? 𐑿 𐑣𐑨𐑝 𐑕𐑳𐑥𐑔𐑦𐑙"
+	line "𐑓 𐑥𐑰?"
 
-	para "<PLAYER> delivered"
-	line "OAK's PARCEL.@"
+	para "<PLAYER> 𐑛𐑦𐑤𐑦𐑝𐑼𐑛"
+	line "·𐑴𐑒𐑕 𐑐𐑸𐑕𐑩𐑤.@"
 	text_end
 
 _OaksLabOak1ParcelThanksText::
 	text_start
-	para "Ah! This is the"
-	line "custom # BALL"
-	cont "I ordered!"
-	cont "Thank you!"
+	para "𐑭! 𐑞𐑦𐑕 𐑦𐑟 𐑞 𐑒𐑳𐑕𐑑𐑩𐑥"
+	line "# 𐑚𐑷𐑤 𐑲 𐑹𐑛𐑼𐑛!"
+	cont "𐑔𐑨𐑙𐑒 𐑿!"
 	done
 
 _OaksLabOak1PokemonAroundTheWorldText::
-	text "#MON around the"
-	line "world wait for"
-	cont "you, <PLAYER>!"
+	text "#𐑥𐑪𐑯 𐑼𐑬𐑯𐑛 𐑞"
+	line "𐑢𐑻𐑤𐑛 𐑢𐑱𐑑 𐑓 𐑿,"
+	cont "<PLAYER>!"
 	done
 
 _OaksLabOak1ReceivedPokeballsText::
-	text "OAK: You can't get"
-	line "detailed data on"
-	cont "#MON by just"
-	cont "seeing them."
+	text "·𐑴𐑒: 𐑿 𐑒𐑭𐑯𐑑 𐑜𐑧𐑑"
+	line "𐑛𐑰𐑑𐑱𐑤𐑛 𐑛𐑱𐑑𐑩 𐑪𐑯"
+	cont "#𐑥𐑪𐑯 𐑚𐑲 𐑡𐑳𐑕𐑑"
+	cont "𐑕𐑰𐑦𐑙 𐑞𐑧𐑥."
 
-	para "You must catch"
-	line "them! Use these"
-	cont "to capture wild"
-	cont "#MON."
+	para "𐑿 𐑥𐑳𐑕𐑑 𐑒𐑨𐑗 𐑞𐑧𐑥!"
+	line "𐑿𐑟 𐑞𐑰𐑟 𐑑 𐑒𐑨𐑐𐑗𐑼"
+	cont "𐑢𐑲𐑤𐑛 #𐑥𐑪𐑯."
 
-	para "<PLAYER> got 5"
-	line "# BALLs!@"
+	para "<PLAYER> 𐑜𐑪𐑑 5"
+	line "# 𐑚𐑷𐑤𐑟!@"
 	text_end
 
 _OaksLabGivePokeballsExplanationText::
 	text_start
-	para "When a wild"
-	line "#MON appears,"
-	cont "it's fair game."
+	para "𐑢𐑧𐑯 𐑩 𐑢𐑲𐑤𐑛"
+	line "#𐑥𐑪𐑯 𐑩𐑐𐑽𐑟, 𐑦𐑑𐑕"
+	cont "𐑓𐑺 𐑜𐑱𐑥."
 
-	para "Just throw a #"
-	line "BALL at it and try"
-	line "to catch it!"
+	para "𐑡𐑳𐑕𐑑 𐑔𐑮𐑴 𐑩 #"
+	line "𐑚𐑷𐑤 𐑨𐑑 𐑦𐑑 𐑯 𐑑𐑮𐑲 𐑑"
+	cont "𐑒𐑨𐑗 𐑦𐑑!"
 
-	para "This won't always"
-	line "work, though."
+	para "𐑞𐑦𐑕 𐑢𐑴𐑯𐑑 𐑷𐑤𐑢𐑱𐑟 𐑢𐑻𐑒,"
+	line "𐑞𐑴."
 
-	para "A healthy #MON"
-	line "could escape. You"
-	cont "have to be lucky!"
+	para "𐑩 𐑣𐑧𐑤𐑔𐑦 #𐑥𐑪𐑯"
+	line "𐑒𐑫𐑛 𐑦𐑕𐑒𐑱𐑐. 𐑿 𐑣𐑨𐑓 𐑑"
+	cont "𐑚𐑰 𐑤𐑳𐑒𐑦!"
 	done
 
 _OaksLabOak1ComeSeeMeSometimesText::
-	text "OAK: Come see me"
-	line "sometimes."
+	text "·𐑴𐑒: 𐑒𐑳𐑥 𐑕𐑰 𐑥𐑰"
+	line "𐑕𐑳𐑥𐑑𐑲𐑥𐑟."
 
-	para "I want to know how"
-	line "your #DEX is"
-	cont "coming along."
+	para "𐑲 𐑢𐑪𐑯𐑑 𐑑 𐑯𐑴 𐑣𐑬 𐑘𐑹"
+	line "#𐑛𐑧𐑒𐑕 𐑦𐑟"
+	cont "𐑒𐑳𐑥𐑦𐑙 𐑩𐑤𐑪𐑙."
 	done
 
 _OaksLabOak1HowIsYourPokedexComingText::
-	text "OAK: Good to see "
-	line "you! How is your "
-	cont "#DEX coming? "
-	cont "Here, let me take"
-	cont "a look!"
+	text "·𐑴𐑒: 𐑜𐑫𐑛 𐑑 𐑕𐑰 𐑿!"
+	line "𐑣𐑬 𐑦𐑟 𐑘𐑹"
+	cont "#𐑛𐑧𐑒𐑕 𐑒𐑳𐑥𐑦𐑙?"
+	cont "𐑣𐑽, 𐑤𐑧𐑑 𐑥𐑰 𐑑𐑱𐑒 𐑩"
+	cont "𐑤𐑫𐑒!"
 	prompt
 
 _OaksLabPokedexText::
-	text "It's encyclopedia-"
-	line "like, but the"
-	cont "pages are blank!"
+	text "𐑦𐑑𐑕 𐑦𐑯𐑕𐑲𐑒𐑤𐑩𐑐𐑰𐑛𐑾-"
+	line "𐑤𐑲𐑒, 𐑚𐑳𐑑 𐑞 𐑐𐑱𐑡𐑩𐑟"
+	cont "𐑸 𐑚𐑤𐑨𐑙𐑒!"
 	done
 
 _OaksLabOak2Text::
@@ -175,190 +168,191 @@ _OaksLabOak2Text::
 	done
 
 _OaksLabGirlText::
-	text "PROF.OAK is the"
-	line "authority on"
-	cont "#MON!"
+	text "·𐑐𐑮𐑪𐑓.𐑴𐑒 𐑦𐑟 𐑞"
+	line "𐑷𐑔𐑪𐑮𐑦𐑑𐑦 𐑪𐑯"
+	cont "#𐑥𐑪𐑯!"
 
-	para "Many #MON"
-	line "trainers hold him"
-	cont "in high regard!"
+	para "𐑥𐑧𐑯𐑦 #𐑥𐑪𐑯"
+	line "𐑑𐑮𐑱𐑯𐑼𐑟 𐑣𐑴𐑤𐑛 𐑣𐑦𐑥 𐑦𐑯"
+	cont "𐑣𐑲 𐑮𐑦𐑜𐑸𐑛!"
 	done
 
 _OaksLabRivalFedUpWithWaitingText::
-	text "<RIVAL>: Gramps!"
-	line "I'm fed up with"
-	cont "waiting!"
+	text "<RIVAL>:"
+	line "·𐑜𐑮𐑨𐑥𐑐𐑕! 𐑲𐑥 𐑓𐑧𐑛"
+	cont "𐑳𐑐 𐑢𐑦𐑞 𐑢𐑱𐑑𐑦𐑙!"
 	done
 
 _OaksLabOakChooseMonText::
-	text "OAK: <RIVAL>?"
-	line "Let me think..."
+	text "·𐑴𐑒: <RIVAL>? 𐑤𐑧𐑑"
+	line "𐑥𐑰 𐑔𐑦𐑙𐑒…"
 
-	para "Oh, that's right,"
-	line "I told you to"
-	cont "come! Just wait!"
+	para "𐑴, 𐑞𐑨𐑑𐑕 𐑮𐑲𐑑, 𐑲"
+	line "𐑑𐑴𐑤𐑛 𐑿 𐑑 𐑒𐑳𐑥!"
+	cont "𐑡𐑳𐑕𐑑 𐑢𐑱𐑑!"
 
-	para "Here, <PLAYER>!"
+	para "𐑣𐑽, <PLAYER>!"
 
-	para "There are 3"
-	line "#MON here!"
+	para "𐑞𐑺 𐑸 3 #𐑥𐑪𐑯"
+	line "𐑣𐑽!"
 
-	para "Haha!"
+	para "𐑣𐑭𐑣𐑭!"
 
-	para "They are inside"
-	line "the # BALLs."
+	para "𐑞𐑱 𐑸 𐑦𐑯𐑕𐑲𐑛 𐑞 #"
+	line "𐑚𐑷𐑤𐑟."
 
-	para "When I was young,"
-	line "I was a serious"
-	cont "#MON trainer!"
+	para "𐑢𐑧𐑯 𐑲 𐑢𐑪𐑟 𐑘𐑳𐑙, 𐑲"
+	line "𐑢𐑪𐑟 𐑩 𐑕𐑽𐑾𐑕"
+	cont "#𐑥𐑪𐑯 𐑑𐑮𐑱𐑯𐑼!"
 
-	para "In my old age, I"
-	line "have only 3 left,"
-	cont "but you can have"
-	cont "one! Choose!"
+	para "𐑦𐑯 𐑥𐑲 𐑴𐑤𐑛 𐑱𐑡, 𐑲"
+	line "𐑣𐑨𐑝 𐑴𐑯𐑤𐑦 3 𐑤𐑧𐑓𐑑,"
+	cont "𐑚𐑳𐑑 𐑿 𐑒𐑨𐑯 𐑣𐑨𐑝"
+	cont "𐑢𐑳𐑯! 𐑗𐑵𐑟!"
 	done
 
 _OaksLabRivalWhatAboutMeText::
-	text "<RIVAL>: Hey!"
-	line "Gramps! What"
-	cont "about me?"
+	text "<RIVAL>: 𐑣𐑱!"
+	line "·𐑜𐑮𐑨𐑥𐑐𐑕! 𐑢𐑪𐑑 𐑩𐑚𐑬𐑑"
+	cont "𐑥𐑰?"
 	done
 
 _OaksLabOakBePatientText::
-	text "OAK: Be patient!"
-	line "<RIVAL>, you can"
-	cont "have one too!"
+	text "·𐑴𐑒: 𐑚𐑰 𐑐𐑱𐑖𐑩𐑯𐑑!"
+	line "<RIVAL>, 𐑿 𐑒𐑨𐑯"
+	cont "𐑣𐑨𐑝 𐑢𐑳𐑯 𐑑𐑵!"
 	done
 
 _OaksLabOakDontGoAwayYetText::
-	text "OAK: Hey! Don't go"
-	line "away yet!"
+	text "·𐑴𐑒: 𐑣𐑱! 𐑛𐑴𐑯𐑑 𐑜𐑴"
+	line "𐑩𐑢𐑱 𐑘𐑧𐑑!"
 	done
 
 _OaksLabRivalIllTakeThisOneText::
-	text "<RIVAL>: I'll take"
-	line "this one, then!"
+	text "<RIVAL>: 𐑲𐑤 𐑑𐑱𐑒"
+	line "𐑞𐑦𐑕 𐑢𐑳𐑯, 𐑞𐑧𐑯!"
 	done
 
 _OaksLabRivalReceivedMonText::
-	text "<RIVAL> received"
-	line "a @"
+	text "<RIVAL> 𐑮𐑦𐑕𐑰𐑝𐑛"
+	line "𐑩 @"
 	text_ram wNameBuffer
 	text "!@"
 	text_end
 
 _OaksLabRivalIllTakeYouOnText::
-	text "<RIVAL>: Wait"
-	line "<PLAYER>!"
-	cont "Let's check out"
-	cont "our #MON!"
+	text "<RIVAL>: 𐑢𐑱𐑑"
+	line "<PLAYER>! 𐑤𐑧𐑑𐑕"
+	cont "𐑗𐑧𐑒 𐑬𐑑 𐑬𐑼"
+	cont "#𐑥𐑪𐑯!"
 
-	para "Come on, I'll take"
-	line "you on!"
+	para "𐑒𐑳𐑥 𐑪𐑯, 𐑲𐑤 𐑑𐑱𐑒 𐑿"
+	line "𐑪𐑯!"
 	done
 
 _OaksLabRivalIPickedTheWrongPokemonText::
-	text "WHAT?"
-	line "Unbelievable!"
-	cont "I picked the"
-	cont "wrong #MON!"
+	text "𐑢𐑪𐑑?"
+	line "𐑳𐑯𐑚𐑦𐑤𐑰𐑝𐑩𐑚𐑩𐑤!"
+	cont "𐑲 𐑐𐑦𐑒𐑑 𐑞 𐑮𐑪𐑙"
+	cont "#𐑥𐑪𐑯!"
 	prompt
 
 _OaksLabRivalAmIGreatOrWhatText::
-	text "<RIVAL>: Yeah! Am"
-	line "I great or what?"
+	text "<RIVAL>: 𐑘𐑧𐑩! 𐑨𐑥"
+	line "𐑲 𐑜𐑮𐑱𐑑 𐑹 𐑢𐑪𐑑?"
 	prompt
 
 _OaksLabRivalSmellYouLaterText::
-	text "<RIVAL>: Okay!"
-	line "I'll make my"
-	cont "#MON fight to"
-	cont "toughen it up!"
+	text "<RIVAL>: 𐑴𐑒𐑱! 𐑲𐑤"
+	line "𐑥𐑱𐑒 𐑥𐑲 #𐑥𐑪𐑯"
+	cont "𐑓𐑲𐑑 𐑑 𐑑𐑳𐑓𐑩𐑯 𐑦𐑑 𐑳𐑐!"
 
-	para "<PLAYER>! Gramps!"
-	line "Smell you later!"
+	para "<PLAYER>!"
+	line "·𐑜𐑮𐑨𐑥𐑐𐑕! 𐑕𐑥𐑧𐑤 𐑿"
+	cont "𐑤𐑱𐑑𐑼!"
 	done
 
 _OaksLabRivalGrampsText::
-	text "<RIVAL>: Gramps!"
+	text "<RIVAL>:"
+	line "·𐑜𐑮𐑨𐑥𐑐𐑕!"
 	done
 
 _OaksLabRivalWhatDidYouCallMeForText::
-	text "<RIVAL>: What did"
-	line "you call me for?"
+	text "<RIVAL>: 𐑢𐑪𐑑 𐑛𐑦𐑛"
+	line "𐑿 𐑒𐑷𐑤 𐑥𐑰 𐑓?"
 	done
 
 _OaksLabOakIHaveARequestText::
-	text "OAK: Oh right! I"
-	line "have a request"
-	cont "of you two."
+	text "·𐑴𐑒: 𐑴 𐑮𐑲𐑑! 𐑲 𐑣𐑨𐑝"
+	line "𐑩 𐑮𐑦𐑒𐑢𐑧𐑕𐑑 𐑝 𐑿 𐑑𐑵."
 	done
 
 _OaksLabOakMyInventionPokedexText::
-	text "On the desk there"
-	line "is my invention,"
-	cont "#DEX!"
+	text "𐑪𐑯 𐑞 𐑛𐑧𐑕𐑒 𐑞𐑺 𐑦𐑟"
+	line "𐑥𐑲 𐑦𐑯𐑝𐑧𐑯𐑖𐑩𐑯,"
+	cont "#𐑛𐑧𐑒𐑕!"
 
-	para "It automatically"
-	line "records data on"
-	cont "#MON you've"
-	cont "seen or caught!"
+	para "𐑦𐑑 𐑷𐑑𐑩𐑥𐑨𐑑𐑦𐑒𐑤𐑦"
+	line "𐑮𐑦𐑒𐑹𐑛𐑟 𐑛𐑱𐑑𐑩 𐑪𐑯"
+	cont "#𐑥𐑪𐑯 𐑿𐑝 𐑕𐑰𐑯 𐑹"
+	cont "𐑒𐑷𐑑!"
 
-	para "It's a hi-tech"
-	line "encyclopedia!"
+	para "𐑦𐑑𐑕 𐑩 𐑣𐑲-𐑑𐑧𐑒"
+	line "𐑦𐑯𐑕𐑲𐑒𐑤𐑩𐑐𐑰𐑛𐑾!"
 	done
 
 _OaksLabOakGotPokedexText::
-	text "OAK: <PLAYER> and"
-	line "<RIVAL>! Take"
-	cont "these with you!"
+	text "·𐑴𐑒: <PLAYER> 𐑯"
+	line "<RIVAL>! 𐑑𐑱𐑒 𐑞𐑰𐑟"
+	cont "𐑢𐑦𐑞 𐑿!"
 
-	para "<PLAYER> got"
-	line "#DEX from OAK!@"
+	para "<PLAYER> 𐑜𐑪𐑑"
+	line "#𐑛𐑧𐑒𐑕 𐑓𐑮𐑪𐑥"
+	cont "·𐑴𐑒!@"
 	text_end
 
 _OaksLabOakThatWasMyDreamText::
-	text "To make a complete"
-	line "guide on all the"
-	cont "#MON in the"
-	cont "world..."
+	text "𐑑 𐑥𐑱𐑒 𐑩 𐑒𐑩𐑥𐑐𐑤𐑰𐑑"
+	line "𐑜𐑲𐑛 𐑪𐑯 𐑷𐑤 𐑞"
+	cont "#𐑥𐑪𐑯 𐑦𐑯 𐑞"
+        cont "𐑢𐑻𐑤𐑛..."
 
-	para "That was my dream!"
+	para "𐑞𐑨𐑑 𐑢𐑪𐑟 𐑥𐑲 𐑛𐑮𐑰𐑥!"
 
-	para "But, I'm too old!"
-	line "I can't do it!"
+	para "𐑚𐑳𐑑, 𐑲𐑥 𐑑𐑵 𐑴𐑤𐑛! 𐑲"
+	line "𐑒𐑭𐑯𐑑 𐑛𐑵 𐑦𐑑!"
 
-	para "So, I want you two"
-	line "to fulfill my"
-	cont "dream for me!"
+	para "𐑕𐑴, 𐑲 𐑢𐑪𐑯𐑑 𐑿 𐑑𐑵 𐑑"
+	line "𐑓𐑫𐑤𐑓𐑦𐑤 𐑥𐑲 𐑛𐑮𐑰𐑥 𐑓"
+	cont "𐑥𐑰!"
 
-	para "Get moving, you"
-	line "two!"
+	para "𐑜𐑧𐑑 𐑥𐑵𐑝𐑦𐑙, 𐑿 𐑑𐑵!"
 
-	para "This is a great"
-	line "undertaking in"
-	cont "#MON history!"
+	para "𐑞𐑦𐑕 𐑦𐑟 𐑩 𐑜𐑮𐑱𐑑"
+	line "𐑳𐑯𐑛𐑼𐑑𐑱𐑒𐑦𐑙 𐑦𐑯"
+	cont "#𐑥𐑪𐑯 𐑣𐑦𐑕𐑑𐑼𐑦!"
 	done
 
 _OaksLabRivalLeaveItAllToMeText::
-	text "<RIVAL>: Alright"
-	line "Gramps! Leave it"
-	cont "all to me!"
+	text "<RIVAL>: 𐑷𐑤𐑮𐑲𐑑"
+	line "·𐑜𐑮𐑨𐑥𐑐𐑕! 𐑤𐑰𐑝 𐑦𐑑 𐑷𐑤"
+	cont "𐑑 𐑥𐑰!"
 
-	para "<PLAYER>, I hate to"
-	line "say it, but I"
-	cont "don't need you!"
+	para "<PLAYER>, 𐑲 𐑣𐑱𐑑 𐑑"
+	line "𐑕𐑱 𐑦𐑑, 𐑚𐑳𐑑 𐑲 𐑛𐑴𐑯𐑑"
+	cont "𐑯𐑰𐑛 𐑿!"
 
-	para "I know! I'll"
-	line "borrow a TOWN MAP"
-	cont "from my sis!"
+	para "𐑲 𐑯𐑴! 𐑲𐑤 𐑚𐑪𐑮𐑴 𐑩"
+	line "𐑑𐑬𐑯 𐑥𐑨𐑐 𐑓𐑮𐑪𐑥 𐑥𐑲"
+	cont "𐑕𐑦𐑕!"
 
-	para "I'll tell her not"
-	line "to lend you one,"
-	cont "<PLAYER>! Hahaha!"
+	para "𐑲𐑤 𐑑𐑧𐑤 𐑣𐑻 𐑯𐑪𐑑 𐑑"
+	line "𐑤𐑧𐑯𐑛 𐑿 𐑢𐑳𐑯,"
+	cont "<PLAYER>!"
+	cont "𐑣𐑭𐑣𐑭𐑣𐑭!"
 	done
 
 _OaksLabScientistText::
-	text "I study #MON as"
-	line "PROF.OAK's AIDE."
+	text "𐑲 𐑕𐑑𐑳𐑛𐑦 #𐑥𐑪𐑯"
+	line "𐑨𐑟 ·𐑐𐑮𐑪𐑓.𐑴𐑒𐑕 𐑱𐑛."
 	done
