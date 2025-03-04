92009d635f453d1232472c03d37b6f86defc89de
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 22:24:45 +0100

    Transliterate the Viridian Forest South Gate

--- text/ViridianForestSouthGate.asm
+++ text/ViridianForestSouthGate.asm
@@ -1,13 +1,12 @@
 _ViridianForestSouthGateGirlText::
-	text "Are you going to"
-	line "VIRIDIAN FOREST?"
-	cont "Be careful, it's"
-	cont "a natural maze!"
+	text "𐑸 𐑿 𐑜𐑴𐑦𐑙 𐑑 ·𐑝𐑦𐑮𐑦𐑛𐑾𐑯"
+	line "𐑓𐑪𐑮𐑦𐑕𐑑? 𐑚𐑰 𐑒𐑺𐑓𐑩𐑤,"
+	cont "𐑦𐑑𐑕 𐑩 𐑯𐑨𐑗𐑼𐑩𐑤 𐑥𐑱𐑟!"
 	done
 
 _ViridianForestSouthGateLittleGirlText::
-	text "RATTATA may be"
-	line "small, but its"
-	cont "bite is wicked!"
-	cont "Did you get one?"
+	text "·𐑮𐑨𐑑𐑩𐑑𐑭 𐑥𐑱 𐑚𐑰"
+	line "𐑕𐑥𐑷𐑤, 𐑚𐑳𐑑 𐑦𐑑𐑕 𐑚𐑲𐑑"
+	cont "𐑦𐑟 𐑢𐑦𐑒𐑩𐑛! 𐑛𐑦𐑛 𐑿"
+	cont "𐑜𐑧𐑑 𐑢𐑳𐑯?"
 	done
