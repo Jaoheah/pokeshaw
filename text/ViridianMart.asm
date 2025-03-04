4fde57a92e92c053d75a4b21a9cda9757982e260
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 18:34:54 +0100

    Transliterate the Viridian mart

--- text/ViridianMart.asm
+++ text/ViridianMart.asm
@@ -1,31 +1,30 @@
 _ViridianMartClerkSayHiToOakText::
-	text "Okay! Say hi to"
-	line "PROF.OAK for me!"
+	text "𐑴𐑒𐑱! 𐑕𐑱 𐑣𐑲 𐑑"
+	line "·𐑐𐑮𐑪𐑓.𐑴𐑒 𐑓 𐑥𐑰!"
 	done
 
 _ViridianMartClerkYouCameFromPalletTownText::
-	text "Hey! You came from"
-	line "PALLET TOWN?"
+	text "𐑣𐑱! 𐑿 𐑒𐑱𐑥 𐑓𐑮𐑪𐑥"
+	line "·𐑐𐑨𐑤𐑩𐑑 𐑑𐑬𐑯?"
 	done
 
 _ViridianMartClerkParcelQuestText::
-	text "You know PROF."
-	line "OAK, right?"
+	text "𐑿 𐑯𐑴 ·𐑐𐑮𐑪𐑓.𐑴𐑒,"
+	line "𐑮𐑲𐑑?"
 
-	para "His order came in."
-	line "Will you take it"
-	cont "to him?"
+	para "𐑣𐑦𐑟 𐑹𐑛𐑼 𐑒𐑱𐑥 𐑦𐑯. 𐑢𐑦𐑤"
+	line "𐑿 𐑑𐑱𐑒 𐑦𐑑 𐑑 𐑣𐑦𐑥?"
 
-	para "<PLAYER> got"
-	line "OAK's PARCEL!@"
+	para "<PLAYER> 𐑜𐑪𐑑 ·𐑴𐑒𐑕"
+	line "𐑐𐑸𐑕𐑩𐑤!@"
 	text_end
 
 _ViridianMartYoungsterText::
-	text "This shop sells"
-	line "many ANTIDOTEs."
+	text "𐑞𐑦𐑕 𐑖𐑪𐑐 𐑕𐑧𐑤𐑟 𐑥𐑧𐑯𐑦"
+	line "𐑨𐑯𐑑𐑦𐑛𐑴𐑑𐑕."
 	done
 
 _ViridianMartCooltrainerMText::
-	text "No! POTIONs are"
-	line "all sold out."
+	text "𐑯𐑴! 𐑐𐑴𐑖𐑩𐑯𐑟 𐑸 𐑷𐑤"
+	line "𐑕𐑴𐑤𐑛 𐑬𐑑."
 	done
