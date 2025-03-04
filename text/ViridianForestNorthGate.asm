e54b0351ec5242d5c749351994376a0fab5715e2
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 22:55:47 +0100

    Transliterate Viridian Forest North Gate

--- text/ViridianForestNorthGate.asm
+++ text/ViridianForestNorthGate.asm
@@ -1,19 +1,18 @@
 _ViridianForestNorthGateSuperNerdText::
-	text "Many #MON live"
-	line "only in forests "
-	cont "and caves."
+	text "𐑥𐑧𐑯𐑦 #𐑥𐑪𐑯 𐑤𐑦𐑝"
+	line "𐑴𐑯𐑤𐑦 𐑦𐑯 𐑓𐑪𐑮𐑦𐑕𐑑𐑕 𐑯"
+	cont "𐑒𐑱𐑝𐑟."
 
-	para "You need to look"
-	line "everywhere to get"
-	cont "different kinds!"
+	para "𐑿 𐑯𐑰𐑛 𐑑 𐑤𐑫𐑒 𐑧𐑝𐑮𐑦𐑢𐑺"
+	line "𐑑 𐑜𐑧𐑑 𐑛𐑦𐑓𐑼𐑩𐑯𐑑 𐑒𐑲𐑯𐑛𐑟!"
 	done
 
 _ViridianForestNorthGateGrampsText::
-	text "Have you noticed"
-	line "the bushes on the"
-	cont "roadside?"
+	text "𐑣𐑨𐑝 𐑿 𐑯𐑴𐑑𐑦𐑕𐑑 𐑞"
+	line "𐑚𐑫𐑖𐑩𐑟 𐑪𐑯 𐑞"
+	cont "𐑮𐑴𐑛𐑕𐑲𐑛?"
 
-	para "They can be cut"
-	line "down by a special"
-	cont "#MON move."
+	para "𐑞𐑱 𐑒𐑨𐑯 𐑚𐑰 𐑒𐑳𐑑 𐑛𐑬𐑯"
+	line "𐑚𐑲 𐑩 𐑕𐑐𐑧𐑖𐑩𐑤"
+	cont "#𐑥𐑪𐑯 𐑥𐑵𐑝."
 	done
