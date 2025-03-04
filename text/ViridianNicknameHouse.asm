1c43f6ef759a940fe92a8b19b1aafd27870684f5
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 18:39:44 +0100

    Transliterate the Viridian nickname house

--- text/ViridianNicknameHouse.asm
+++ text/ViridianNicknameHouse.asm
@@ -1,23 +1,22 @@
 _ViridianNicknameHouseBaldingGuyText::
-	text "Coming up with"
-	line "nicknames is fun,"
-	cont "but hard."
+	text "𐑒𐑳𐑥𐑦𐑙 𐑳𐑐 𐑢𐑦𐑞"
+	line "𐑯𐑦𐑒𐑯𐑱𐑥𐑟 𐑦𐑟 𐑓𐑳𐑯,"
+	cont "𐑚𐑳𐑑 𐑣𐑸𐑛."
 
-	para "Simple names are"
-	line "the easiest to"
-	cont "remember."
+	para "𐑕𐑦𐑥𐑐𐑩𐑤 𐑯𐑱𐑥𐑟 𐑸 𐑞"
+	line "𐑰𐑟𐑦𐑩𐑕𐑑 𐑑 𐑮𐑦𐑥𐑧𐑥𐑚𐑼."
 	done
 
 _ViridianNicknameHouseLittleGirlText::
-	text "My Daddy loves"
-	line "#MON too."
+	text "𐑥𐑲 𐑛𐑨𐑛𐑦 𐑤𐑳𐑝𐑟"
+	line "#𐑥𐑪𐑯 𐑑𐑵."
 	done
 
 _ViridianNicknameHouseSpearowText::
-	text "SPEARY: Tetweet!"
+	text "𐑕𐑐𐑽𐑦: 𐑑𐑰𐑑𐑢𐑰𐑑!"
 	done
 
 _ViridianNicknameHouseSpearySignText::
-	text "SPEAROW"
-	line "Name: SPEARY"
+	text "𐑕𐑐𐑽𐑴"
+	line "𐑯𐑱𐑥: ·𐑕𐑐𐑽𐑦"
 	done
