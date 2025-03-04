55db2fc4ef5b6dd371e7391c324147d82884f226
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 11:58:47 +0100

    Transliterate the Route 1 text

--- text/Route1.asm
+++ text/Route1.asm
@@ -1,50 +1,48 @@
 _Route1Youngster1MartSampleText::
-	text "Hi! I work at a"
-	line "#MON MART."
+	text "𐑣𐑲! 𐑲 𐑢𐑻𐑒 𐑨𐑑 𐑩"
+	line "#𐑥𐑪𐑯 𐑥𐑸𐑑."
 
-	para "It's a convenient"
-	line "shop, so please"
-	cont "visit us in"
-	cont "VIRIDIAN CITY."
+	para "𐑦𐑑𐑕 𐑩 𐑒𐑩𐑯𐑝𐑰𐑯𐑾𐑯𐑑 𐑖𐑪𐑐,"
+	line "𐑕𐑴 𐑐𐑤𐑰𐑟 𐑝𐑦𐑟𐑦𐑑 𐑳𐑕 𐑦𐑯"
+	cont "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑕𐑦𐑑𐑦."
 
-	para "I know, I'll give"
-	line "you a sample!"
-	cont "Here you go!"
+	para "𐑲 𐑯𐑴, 𐑲𐑤 𐑜𐑦𐑝 𐑿 𐑩"
+	line "𐑕𐑭𐑥𐑐𐑩𐑤! 𐑣𐑽 𐑿 𐑜𐑴!"
 	prompt
 
 _Route1Youngster1GotPotionText::
-	text "<PLAYER> got"
+	text "<PLAYER> 𐑜𐑪𐑑"
 	line "@"
 	text_ram wStringBuffer
 	text "!@"
 	text_end
 
 _Route1Youngster1AlsoGotPokeballsText::
-	text "We also carry"
-	line "# BALLs for"
-	cont "catching #MON!"
+	text "𐑢𐑰 𐑷𐑤𐑕𐑴 𐑒𐑨𐑮𐑦 #"
+	line "𐑚𐑷𐑤𐑟 𐑓 𐑒𐑨𐑗𐑦𐑙"
+	cont "#𐑥𐑪𐑯!"
 	done
 
 _Route1Youngster1NoRoomText::
-	text "You have too much"
-	line "stuff with you!"
+	text "𐑿 𐑣𐑨𐑝 𐑑𐑵 𐑥𐑳𐑗 𐑕𐑑𐑳𐑓"
+	line "𐑢𐑦𐑞 𐑿!"
 	done
 
 _Route1Youngster2Text::
-	text "See those ledges"
-	line "along the road?"
+	text "𐑕𐑰 𐑞𐑴𐑟 𐑤𐑧𐑡𐑩𐑟 𐑩𐑤𐑪𐑙"
+	line "𐑞 𐑮𐑴𐑛?"
 
-	para "It's a bit scary,"
-	line "but you can jump"
-	cont "from them."
+	para "𐑦𐑑𐑕 𐑩 𐑚𐑦𐑑 𐑕𐑒𐑺𐑦, 𐑚𐑳𐑑"
+	line "𐑿 𐑒𐑨𐑯 𐑡𐑳𐑥𐑐 𐑓𐑮𐑪𐑥"
+	cont "𐑞𐑧𐑥."
 
-	para "You can get back"
-	line "to PALLET TOWN"
-	cont "quicker that way."
+	para "𐑿 𐑒𐑨𐑯 𐑜𐑧𐑑 𐑚𐑨𐑒 𐑑"
+	line "·𐑐𐑨𐑤𐑩𐑑 𐑑𐑬𐑯 𐑒𐑢𐑦𐑒𐑼"
+	cont "𐑞𐑨𐑑 𐑢𐑱."
 	done
 
 _Route1SignText::
-	text "ROUTE 1"
-	line "PALLET TOWN -"
-	cont "VIRIDIAN CITY"
+	text "𐑮𐑵𐑑 1"
+	line "·𐑐𐑨𐑤𐑩𐑑 𐑑𐑬𐑯 -"
+	cont "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑕𐑦𐑑𐑦"
 	done
