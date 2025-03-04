3b30fe104e4b6d22c00fa7762414516bbdc043cb
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-04 14:42:26 +0100

    Transliterate Viridian City text

--- text/ViridianCity.asm
+++ text/ViridianCity.asm
@@ -1,181 +1,170 @@
 _ViridianCityYoungster1Text::
-	text "Those # BALLs"
-	line "at your waist!"
-	cont "You have #MON!"
+	text "𐑞𐑴𐑟 # 𐑚𐑷𐑤𐑟 𐑨𐑑"
+	line "𐑘𐑹 𐑢𐑱𐑕𐑑! 𐑿 𐑣𐑨𐑝"
+	cont "#𐑥𐑪𐑯!"
 
-	para "It's great that"
-	line "you can carry and"
-	cont "use #MON any"
-	cont "time, anywhere!"
+	para "𐑦𐑑𐑕 𐑜𐑮𐑱𐑑 𐑞𐑨𐑑 𐑿 𐑒𐑨𐑯"
+	line "𐑒𐑨𐑮𐑦 𐑯 𐑿𐑟 #𐑥𐑪𐑯"
+	cont "𐑧𐑯𐑦 𐑑𐑲𐑥, 𐑧𐑯𐑦𐑢𐑺!"
 	done
 
 _ViridianCityGambler1GymAlwaysClosedText::
-	text "This #MON GYM"
-	line "is always closed."
+	text "𐑞𐑦𐑕 #𐑥𐑪𐑯 𐑡𐑦𐑥 𐑦𐑟"
+	line "𐑷𐑤𐑢𐑱𐑟 𐑒𐑤𐑴𐑟𐑛."
 
-	para "I wonder who the"
-	line "LEADER is?"
+	para "𐑲 𐑢𐑳𐑯𐑛𐑼 𐑣𐑵 𐑞 𐑤𐑰𐑛𐑼"
+	line "𐑦𐑟?"
 	done
 
 _ViridianCityGambler1GymLeaderReturnedText::
-	text "VIRIDIAN GYM's"
-	line "LEADER returned!"
+	text "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑡𐑦𐑥𐑟"
+	line "𐑤𐑰𐑛𐑼 𐑮𐑦𐑑𐑻𐑯𐑛!"
 	done
 
 _ViridianCityYoungster2YouWantToKnowAboutText::
-	text "You want to know"
-	line "about the 2 kinds"
-	cont "of caterpillar"
-	cont "#MON?"
+	text "𐑿 𐑢𐑪𐑯𐑑 𐑑 𐑯𐑴 𐑩𐑚𐑬𐑑 𐑞"
+	line "2 𐑒𐑲𐑯𐑛𐑟 𐑝"
+	cont "𐑒𐑨𐑑𐑼𐑐𐑦𐑤𐑼 #𐑥𐑪𐑯?"
 	done
 
 ViridianCityYoungster2OkThenText::
-	text "Oh, OK then!"
+	text "𐑴, 𐑴𐑒𐑱 𐑞𐑧𐑯!"
 	done
 
 ViridianCityYoungster2CaterpieAndWeedleDescriptionText::
-	text "CATERPIE has no"
-	line "poison, but"
-	cont "WEEDLE does."
+	text "·𐑒𐑨𐑑𐑼𐑐𐑦 𐑣𐑨𐑟 𐑯𐑴"
+	line "𐑐𐑶𐑟𐑩𐑯, 𐑚𐑳𐑑 ·𐑢𐑰𐑛𐑩𐑤"
+	cont "𐑛𐑳𐑟."
 
-	para "Watch out for its"
-	line "POISON STING!"
+	para "𐑢𐑪𐑗 𐑬𐑑 𐑓 𐑦𐑑𐑕 𐑐𐑶𐑟𐑩𐑯"
+	line "𐑕𐑑𐑦𐑙!"
 	done
 
 _ViridianCityGirlHasntHadHisCoffeeYetText::
-	text "Oh Grandpa! Don't"
-	line "be so mean!"
-	cont "He hasn't had his"
-	cont "coffee yet."
+	text "𐑴 ·𐑜𐑮𐑨𐑯𐑛𐑐𐑭! 𐑛𐑴𐑯𐑑 𐑚𐑰"
+	line "𐑕𐑴 𐑥𐑰𐑯! 𐑣𐑰 𐑣𐑨𐑟𐑩𐑯𐑑"
+	cont "𐑣𐑨𐑛 𐑣𐑦𐑟 𐑒𐑪𐑓𐑦 𐑘𐑧𐑑."
 	done
 
 _ViridianCityGirlWhenIGoShopText::
-	text "When I go shop in"
-	line "PEWTER CITY, I"
-	cont "have to take the"
-	cont "winding trail in"
-	cont "VIRIDIAN FOREST."
+	text "𐑢𐑧𐑯 𐑲 𐑜𐑴 𐑖𐑪𐑐 𐑦𐑯"
+	line "·𐑐𐑿𐑑𐑼 𐑕𐑦𐑑𐑦, 𐑲 𐑣𐑨𐑓"
+	cont "𐑑 𐑑𐑱𐑒 𐑞 𐑢𐑲𐑯𐑛𐑦𐑙"
+	cont "𐑑𐑮𐑱𐑤 𐑦𐑯 ·𐑝𐑦𐑮𐑦𐑛𐑾𐑯"
+	cont "𐑓𐑪𐑮𐑦𐑕𐑑."
 	done
 
 _ViridianCityOldManSleepyPrivatePropertyText::
-	text "You can't go"
-	line "through here!"
+	text "𐑿 𐑒𐑭𐑯𐑑 𐑜𐑴 𐑔𐑮𐑵 𐑣𐑽!"
 
-	para "This is private"
-	line "property!"
+	para "𐑞𐑦𐑕 𐑦𐑟 𐑐𐑮𐑲𐑝𐑩𐑑"
+	line "𐑐𐑮𐑪𐑐𐑼𐑑𐑦!"
 	done
 
 ViridianCityFisherYouCanHaveThisText::
-	text "Yawn!"
-	line "I must have dozed"
-	cont "off in the sun."
+	text "𐑘𐑷𐑯!"
+	line "𐑲 𐑥𐑳𐑕𐑑 𐑣𐑨𐑝 𐑛𐑴𐑟𐑛 "
+	cont "𐑪𐑓 𐑦𐑯 𐑞 𐑕𐑳𐑯."
 
-	para "I had this dream"
-	line "about a DROWZEE"
-	cont "eating my dream."
-	cont "What's this?"
-	cont "Where did this TM"
-	cont "come from?"
+	para "𐑲 𐑣𐑨𐑛 𐑞𐑦𐑕 𐑛𐑮𐑰𐑥 𐑩𐑚𐑬𐑑"
+	line "𐑩 𐑛𐑮𐑬𐑟𐑦 𐑰𐑑𐑦𐑙 𐑥𐑲"
+	cont "𐑛𐑮𐑰𐑥. 𐑢𐑪𐑑𐑕 𐑞𐑦𐑕? 𐑢𐑺"
+	cont "𐑛𐑦𐑛 𐑞𐑦𐑕 ⸰𐑑𐑥 𐑒𐑳𐑥"
+	cont "𐑓𐑮𐑪𐑥?"
 
-	para "This is spooky!"
-	line "Here, you can"
-	cont "have this TM."
+	para "𐑞𐑦𐑕 𐑦𐑟 𐑕𐑐𐑵𐑒𐑦! 𐑣𐑽, 𐑿"
+	line "𐑒𐑨𐑯 𐑣𐑨𐑝 𐑞𐑦𐑕 ⸰𐑑𐑥."
 	prompt
 
 _ViridianCityFisherReceivedTM42Text::
-	text "<PLAYER> received"
-	line "TM42!@"
+	text "<PLAYER> 𐑮𐑦𐑕𐑰𐑝𐑛"
+	line "⸰𐑑𐑥42!@"
 	text_end
 
 _ViridianCityFisherTM42ExplanationText::
-	text "TM42 contains"
-	line "DREAM EATER..."
-	cont "...Snore..."
+	text "⸰𐑑𐑥42 𐑒𐑩𐑯𐑑𐑱𐑯𐑟"
+	line "«𐑛𐑮𐑰𐑥 𐑰𐑑𐑼»..."
+	cont "...𐑕𐑯𐑹..."
 	done
 
 _ViridianCityFisherTM42NoRoomText::
-	text "You have too much"
-	line "stuff already."
+	text "𐑿 𐑣𐑨𐑝 𐑑𐑵 𐑥𐑳𐑗 𐑕𐑑𐑳𐑓"
+	line "𐑷𐑤𐑮𐑧𐑛𐑦."
 	done
 
 _ViridianCityOldManHadMyCoffeeNowText::
-	text "Ahh, I've had my"
-	line "coffee now and I"
-	cont "feel great!"
+	text "𐑭~~, 𐑲𐑝 𐑣𐑨𐑛 𐑥𐑲"
+	line "𐑒𐑪𐑓𐑦 𐑯𐑬 𐑯 𐑲 𐑓𐑰𐑤"
+	cont "𐑜𐑮𐑱𐑑!"
 
-	para "Sure you can go"
-	line "through!"
+	para "𐑖𐑫𐑼 𐑿 𐑒𐑨𐑯 𐑜𐑴 𐑔𐑮𐑵!"
 
-	para "Are you in a"
-	line "hurry?"
+	para "𐑸 𐑿 𐑦𐑯 𐑩 𐑣𐑳𐑮𐑦?"
 	done
 
 _ViridianCityOldManKnowHowToCatchPokemonText::
-	text "I see you're using"
-	line "a #DEX."
+	text "𐑲 𐑕𐑰 𐑿𐑼 𐑿𐑟𐑦𐑙 𐑩"
+	line "#𐑛𐑧𐑒𐑕."
 
-	para "When you catch a"
-	line "#MON, #DEX"
-	cont "is automatically"
-	cont "updated."
+	para "𐑢𐑧𐑯 𐑿 𐑒𐑨𐑗 𐑩"
+	line "#𐑥𐑪𐑯, #𐑛𐑧𐑒𐑕"
+	cont "𐑦𐑟 𐑷𐑑𐑩𐑥𐑨𐑑𐑦𐑒𐑤𐑦"
+	cont "𐑳𐑐𐑛𐑱𐑑𐑩𐑛."
 
-	para "What? Don't you"
-	line "know how to catch"
-	cont "#MON?"
+	para "𐑢𐑪𐑑? 𐑛𐑴𐑯𐑑 𐑿 𐑯𐑴 𐑣𐑬 𐑑"
+	line "𐑒𐑨𐑗 #𐑥𐑪𐑯?"
 
-	para "I'll show you"
-	line "how to then."
+	para "𐑲𐑤 𐑖𐑴 𐑿 𐑣𐑬 𐑑 𐑞𐑧𐑯."
 	done
 
 _ViridianCityOldManTimeIsMoneyText::
-	text "Time is money..."
-	line "Go along then."
+	text "·𐑑𐑲𐑥 𐑦𐑟 𐑥𐑳𐑯𐑦..."
+	line "𐑜𐑴 𐑩𐑤𐑪𐑙 𐑞𐑧𐑯..."
 	done
 
 _ViridianCityOldManYouNeedToWeakenTheTargetText::
-	text "First, you need"
-	line "to weaken the"
-	cont "target #MON."
+	text "𐑓𐑻𐑕𐑑, 𐑿 𐑯𐑰𐑛 𐑑"
+	line "𐑢𐑰𐑒𐑩𐑯 𐑞 𐑑𐑸𐑜𐑩𐑑"
+	cont "#𐑥𐑪𐑯."
 	done
 
 _ViridianCitySignText::
-	text "VIRIDIAN CITY "
-	line "The Eternally"
-	cont "Green Paradise"
+	text "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑕𐑦𐑑𐑦"
+	line "𐑞 ·𐑦𐑑𐑻𐑯𐑩𐑤𐑦 𐑜𐑮𐑰𐑯"
+	cont "𐑐𐑨𐑮𐑩𐑛𐑲𐑕"
 	done
 
 _ViridianCityTrainerTips1Text::
-	text "TRAINER TIPS"
+	text "𐑑𐑮𐑱𐑯𐑼 𐑑𐑦𐑐𐑕"
 
-	para "Catch #MON"
-	line "and expand your"
-	cont "collection!"
+	para "𐑒𐑨𐑗 #𐑥𐑪𐑯 𐑯"
+	line "𐑦𐑒𐑕𐑐𐑨𐑯𐑛 𐑘𐑹"
+	cont "𐑒𐑩𐑤𐑧𐑒𐑖𐑩𐑯!"
 
-	para "The more you have,"
-	line "the easier it is"
-	cont "to fight!"
+	para "𐑞 𐑥𐑹 𐑿 𐑣𐑨𐑝, 𐑞 𐑰𐑟𐑦𐑼"
+	line "𐑦𐑑 𐑦𐑟 𐑑 𐑓𐑲𐑑!"
 	done
 
 _ViridianCityTrainerTips2Text::
-	text "TRAINER TIPS"
+	text "𐑑𐑮𐑱𐑯𐑼 𐑑𐑦𐑐𐑕"
 
-	para "The battle moves"
-	line "of #MON are"
-	cont "limited by their"
-	cont "POWER POINTs, PP."
+	para "𐑞 𐑚𐑨𐑑𐑩𐑤 𐑥𐑵𐑝𐑟 𐑝"
+	line "#𐑥𐑪𐑯 𐑸 𐑤𐑦𐑥𐑦𐑑𐑩𐑛"
+	cont "𐑚𐑲 𐑞𐑺 𐑐𐑬𐑼 𐑐𐑶𐑯𐑑𐑕,"
+	cont "⸰𐑐𐑐."
 
-	para "To replenish PP,"
-	line "rest your tired"
-	cont "#MON at a"
-	cont "#MON CENTER!"
+	para "𐑑 𐑮𐑦𐑐𐑤𐑧𐑯𐑦𐑖 ⸰𐑐𐑐,"
+	line "𐑮𐑧𐑕𐑑 𐑘𐑹 𐑑𐑲𐑼𐑛"
+	cont "#𐑥𐑪𐑯 𐑨𐑑 𐑩"
+	cont "#𐑥𐑪𐑯 𐑕𐑧𐑯𐑑𐑼!"
 	done
 
 _ViridianCityGymSignText::
-	text "VIRIDIAN CITY"
-	line "#MON GYM"
+	text "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑕𐑦𐑑𐑦"
+	line "#𐑥𐑪𐑯 𐑡𐑦𐑥"
 	done
 
 _ViridianCityGymLockedText::
-	text "The GYM's doors"
-	line "are locked..."
+	text "𐑞 ·𐑡𐑦𐑥𐑟 𐑛𐑹𐑟 𐑸"
+	line "𐑤𐑪𐑒𐑑..."
 	done
