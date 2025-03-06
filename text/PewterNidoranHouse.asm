a24f95eb5edd1328051ffbea3dee68a7432d98e5
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 12:26:41 +0100

    Transliterate the Pewter Nidoran House

--- text/PewterNidoranHouse.asm
+++ text/PewterNidoranHouse.asm
@@ -1,25 +1,24 @@
 _PewterNidoranHouseNidoranText::
-	text "NIDORAN: Bowbow!@"
+	text "·𐑯𐑦𐑛𐑼𐑨𐑯: 𐑚𐑬𐑚𐑬!@"
 	text_end
 
 _PewterNidoranHouseLittleBoyText::
-	text "NIDORAN sit!"
+	text "·𐑯𐑦𐑛𐑼𐑨𐑯 𐑕𐑦𐑑!"
 	done
 
 _PewterNidoranHouseMiddleAgedManText::
-	text "Our #MON's an"
-	line "outsider, so it's"
-	cont "hard to handle."
+	text "𐑬𐑼 #𐑥𐑪𐑯𐑟 𐑩𐑯"
+	line "𐑬𐑑𐑕𐑲𐑛𐑼, 𐑕𐑴 𐑦𐑑𐑕 𐑣𐑸𐑛"
+	cont "𐑑 𐑣𐑨𐑯𐑛𐑩𐑤."
 
-	para "An outsider is a"
-	line "#MON that you"
-	cont "get in a trade."
+	para "𐑩𐑯 𐑬𐑑𐑕𐑲𐑛𐑼 𐑦𐑟 𐑩"
+	line "#𐑥𐑪𐑯 𐑞𐑨𐑑 𐑿 𐑜𐑧𐑑"
+	cont "𐑦𐑯 𐑩 𐑑𐑮𐑱𐑛."
 
-	para "It grows fast, but"
-	line "it may ignore an"
-	cont "unskilled trainer"
-	cont "in battle!"
+	para "𐑦𐑑 𐑜𐑮𐑴𐑟 𐑓𐑭𐑕𐑑, 𐑚𐑳𐑑 𐑦𐑑"
+	line "𐑥𐑱 𐑦𐑜𐑯𐑹 𐑩𐑯 𐑳𐑯𐑕𐑒𐑦𐑤𐑛"
+	cont "𐑑𐑮𐑱𐑯𐑼 𐑦𐑯 𐑚𐑨𐑑𐑩𐑤!"
 
-	para "If only we had"
-	line "some BADGEs..."
+	para "𐑦𐑓 𐑴𐑯𐑤𐑦 𐑢𐑰 𐑣𐑨𐑛 𐑕𐑳𐑥"
+	line "𐑚𐑨𐑡𐑩𐑟..."
 	done
