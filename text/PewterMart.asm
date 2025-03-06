3967af4fc25b1c21c6cdf32dfcecf0a7147f1d33
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 12:15:26 +0100

    Transliterate Pewter Mart

--- text/PewterMart.asm
+++ text/PewterMart.asm
@@ -1,17 +1,16 @@
 _PewterMartYoungsterText::
-	text "A shady, old man"
-	line "got me to buy"
-	cont "this really weird"
-	cont "fish #MON!"
+	text "𐑩 𐑖𐑱𐑛𐑦, 𐑴𐑤𐑛 𐑥𐑨𐑯"
+	line "𐑜𐑪𐑑 𐑥𐑰 𐑑 𐑚𐑲 𐑞𐑦𐑕"
+	cont "𐑮𐑾𐑤𐑦 𐑢𐑽𐑛 𐑓𐑦𐑖"
+	cont "#𐑥𐑪𐑯!"
 
-	para "It's totally weak"
-	line "and it cost ¥500!"
+	para "𐑦𐑑𐑕 𐑑𐑴𐑑𐑩𐑤𐑦 𐑢𐑰𐑒 𐑯 𐑦𐑑"
+	line "𐑒𐑪𐑕𐑑 ¥500!"
 	done
 
 _PewterMartSuperNerdText::
-	text "Good things can"
-	line "happen if you"
-	cont "raise #MON"
-	cont "diligently, even"
-	cont "the weak ones!"
+	text "𐑜𐑫𐑛 𐑔𐑦𐑙𐑟 𐑒𐑨𐑯 𐑣𐑨𐑐𐑩𐑯"
+	line "𐑦𐑓 𐑿 𐑮𐑱𐑟 #𐑥𐑪𐑯"
+	cont "𐑛𐑦𐑤𐑦𐑡𐑩𐑯𐑑𐑤𐑦, 𐑰𐑝𐑩𐑯 𐑞"
+	cont "𐑢𐑰𐑒 𐑢𐑳𐑯𐑟!"
 	done
