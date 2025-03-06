e26098b2f675d6236173c305588cc5998b54af1c
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 19:06:19 +0100

    Transliterate move names

--- data/moves/field_move_names.asm
+++ data/moves/field_move_names.asm
@@ -1,11 +1,11 @@
 ; see also FieldMoveDisplayData
 FieldMoveNames:
-	db "CUT@"
-	db "FLY@"
+	db "𐑒𐑳𐑑@" ; CUT
+	db "𐑓𐑤𐑲@" ; FLY
 	db "@"
-	db "SURF@"
-	db "STRENGTH@"
-	db "FLASH@"
-	db "DIG@"
-	db "TELEPORT@"
-	db "SOFTBOILED@"
+	db "𐑕𐑻𐑓@" ; SURF
+	db "𐑕𐑑𐑮𐑧𐑙𐑔@" ; STRENGTH
+	db "𐑓𐑤𐑨𐑖@" ; FLASH
+	db "𐑛𐑦𐑜@" ; DIG
+	db "𐑑𐑧𐑤𐑦𐑐𐑹𐑑@" ; TELEPORT
+	db "𐑕𐑪𐑓𐑑 𐑚𐑶𐑤𐑛@" ; SOFTBOILED
--- data/moves/names.asm
+++ data/moves/names.asm
@@ -1,168 +1,168 @@
 MoveNames::
 	list_start
-	li "POUND"
-	li "KARATE CHOP"
-	li "DOUBLESLAP"
-	li "COMET PUNCH"
-	li "MEGA PUNCH"
-	li "PAY DAY"
-	li "FIRE PUNCH"
-	li "ICE PUNCH"
-	li "THUNDERPUNCH"
-	li "SCRATCH"
-	li "VICEGRIP"
-	li "GUILLOTINE"
-	li "RAZOR WIND"
-	li "SWORDS DANCE"
-	li "CUT"
-	li "GUST"
-	li "WING ATTACK"
-	li "WHIRLWIND"
-	li "FLY"
-	li "BIND"
-	li "SLAM"
-	li "VINE WHIP"
-	li "STOMP"
-	li "DOUBLE KICK"
-	li "MEGA KICK"
-	li "JUMP KICK"
-	li "ROLLING KICK"
-	li "SAND-ATTACK"
-	li "HEADBUTT"
-	li "HORN ATTACK"
-	li "FURY ATTACK"
-	li "HORN DRILL"
-	li "TACKLE"
-	li "BODY SLAM"
-	li "WRAP"
-	li "TAKE DOWN"
-	li "THRASH"
-	li "DOUBLE-EDGE"
-	li "TAIL WHIP"
-	li "POISON STING"
-	li "TWINEEDLE"
-	li "PIN MISSILE"
-	li "LEER"
-	li "BITE"
-	li "GROWL"
-	li "ROAR"
-	li "SING"
-	li "SUPERSONIC"
-	li "SONICBOOM"
-	li "DISABLE"
-	li "ACID"
-	li "EMBER"
-	li "FLAMETHROWER"
-	li "MIST"
-	li "WATER GUN"
-	li "HYDRO PUMP"
-	li "SURF"
-	li "ICE BEAM"
-	li "BLIZZARD"
-	li "PSYBEAM"
-	li "BUBBLEBEAM"
-	li "AURORA BEAM"
-	li "HYPER BEAM"
-	li "PECK"
-	li "DRILL PECK"
-	li "SUBMISSION"
-	li "LOW KICK"
-	li "COUNTER"
-	li "SEISMIC TOSS"
-	li "STRENGTH"
-	li "ABSORB"
-	li "MEGA DRAIN"
-	li "LEECH SEED"
-	li "GROWTH"
-	li "RAZOR LEAF"
-	li "SOLARBEAM"
-	li "POISONPOWDER"
-	li "STUN SPORE"
-	li "SLEEP POWDER"
-	li "PETAL DANCE"
-	li "STRING SHOT"
-	li "DRAGON RAGE"
-	li "FIRE SPIN"
-	li "THUNDERSHOCK"
-	li "THUNDERBOLT"
-	li "THUNDER WAVE"
-	li "THUNDER"
-	li "ROCK THROW"
-	li "EARTHQUAKE"
-	li "FISSURE"
-	li "DIG"
-	li "TOXIC"
-	li "CONFUSION"
-	li "PSYCHIC"
-	li "HYPNOSIS"
-	li "MEDITATE"
-	li "AGILITY"
-	li "QUICK ATTACK"
-	li "RAGE"
-	li "TELEPORT"
-	li "NIGHT SHADE"
-	li "MIMIC"
-	li "SCREECH"
-	li "DOUBLE TEAM"
-	li "RECOVER"
-	li "HARDEN"
-	li "MINIMIZE"
-	li "SMOKESCREEN"
-	li "CONFUSE RAY"
-	li "WITHDRAW"
-	li "DEFENSE CURL"
-	li "BARRIER"
-	li "LIGHT SCREEN"
-	li "HAZE"
-	li "REFLECT"
-	li "FOCUS ENERGY"
-	li "BIDE"
-	li "METRONOME"
-	li "MIRROR MOVE"
-	li "SELFDESTRUCT"
-	li "EGG BOMB"
-	li "LICK"
-	li "SMOG"
-	li "SLUDGE"
-	li "BONE CLUB"
-	li "FIRE BLAST"
-	li "WATERFALL"
-	li "CLAMP"
-	li "SWIFT"
-	li "SKULL BASH"
-	li "SPIKE CANNON"
-	li "CONSTRICT"
-	li "AMNESIA"
-	li "KINESIS"
-	li "SOFTBOILED"
-	li "HI JUMP KICK"
-	li "GLARE"
-	li "DREAM EATER"
-	li "POISON GAS"
-	li "BARRAGE"
-	li "LEECH LIFE"
-	li "LOVELY KISS"
-	li "SKY ATTACK"
-	li "TRANSFORM"
-	li "BUBBLE"
-	li "DIZZY PUNCH"
-	li "SPORE"
-	li "FLASH"
-	li "PSYWAVE"
-	li "SPLASH"
-	li "ACID ARMOR"
-	li "CRABHAMMER"
-	li "EXPLOSION"
-	li "FURY SWIPES"
-	li "BONEMERANG"
-	li "REST"
-	li "ROCK SLIDE"
-	li "HYPER FANG"
-	li "SHARPEN"
-	li "CONVERSION"
-	li "TRI ATTACK"
-	li "SUPER FANG"
-	li "SLASH"
-	li "SUBSTITUTE"
-	li "STRUGGLE"
+	li "𐑐𐑬𐑯𐑛" ; POUND
+	li "𐑒𐑼𐑭𐑑𐑦 𐑗𐑪𐑐" ; KARATE CHOP
+	li "𐑛𐑳𐑚𐑩𐑤 𐑕𐑤𐑨𐑐" ; DOUBLESLAP
+	li "𐑒𐑪𐑥𐑩𐑑 𐑐𐑳𐑯𐑗" ; COMET PUNCH
+	li "𐑥𐑧𐑜𐑩 𐑐𐑳𐑯𐑗" ; MEGA PUNCH
+	li "𐑐𐑱 𐑛𐑱" ; PAY DAY
+	li "𐑓𐑲𐑼 𐑐𐑳𐑯𐑗" ; FIRE PUNCH
+	li "𐑲𐑕 𐑐𐑳𐑯𐑗" ; ICE PUNCH
+	li "𐑔𐑳𐑯𐑛𐑼 𐑐𐑳𐑯𐑗" ; THUNDERPUNCH
+	li "𐑕𐑒𐑮𐑨𐑗" ; SCRATCH
+	li "𐑝𐑲𐑕 𐑜𐑮𐑦𐑐" ; VICEGRIP
+	li "𐑜𐑦𐑤𐑩𐑑𐑰𐑯" ; GUILLOTINE
+	li "𐑮𐑱𐑟𐑼 𐑢𐑦𐑯𐑛" ; RAZOR WIND
+	li "𐑕𐑹𐑛𐑟 𐑛𐑭𐑯𐑕" ; SWORDS DANCE
+	li "𐑒𐑳𐑑" ; CUT
+	li "𐑜𐑳𐑕𐑑" ; GUST
+	li "𐑢𐑦𐑙 𐑩𐑑𐑨𐑒" ; WING ATTACK
+	li "𐑢𐑻𐑤𐑢𐑦𐑯𐑛" ; WHIRLWIND
+	li "𐑓𐑤𐑲" ; FLY
+	li "𐑚𐑲𐑯𐑛" ; BIND
+	li "𐑕𐑤𐑨𐑥" ; SLAM
+	li "𐑝𐑲𐑯 𐑢𐑦𐑐" ; VINE WHIP
+	li "𐑕𐑑𐑪𐑥𐑐" ; STOMP
+	li "𐑛𐑳𐑚𐑩𐑤 𐑒𐑦𐑒" ; DOUBLE KICK
+	li "𐑥𐑧𐑜𐑩 𐑒𐑦𐑒" ; MEGA KICK
+	li "𐑡𐑳𐑥𐑐 𐑒𐑦𐑒" ; JUMP KICK
+	li "𐑮𐑴𐑤𐑦𐑙 𐑒𐑦𐑒" ; ROLLING KICK
+	li "𐑕𐑨𐑯𐑛-𐑩𐑑𐑨𐑒" ; SAND-ATTACK
+	li "𐑣𐑧𐑛𐑚𐑳𐑑" ; HEADBUTT
+	li "𐑣𐑹𐑯 𐑩𐑑𐑨𐑒" ; HORN ATTACK
+	li "𐑓𐑘𐑫𐑼𐑦 𐑩𐑑𐑨𐑒" ; FURY ATTACK
+	li "𐑣𐑹𐑯 𐑛𐑮𐑦𐑤" ; HORN DRILL
+	li "𐑑𐑨𐑒𐑩𐑤" ; TACKLE
+	li "𐑚𐑪𐑛𐑦 𐑕𐑤𐑨𐑥" ; BODY SLAM
+	li "𐑮𐑨𐑐" ; WRAP
+	li "𐑑𐑱𐑒 𐑛𐑬𐑯" ; TAKE DOWN
+	li "𐑔𐑮𐑨𐑖" ; THRASH
+	li "𐑛𐑳𐑚𐑩𐑤-𐑧𐑡" ; DOUBLE-EDGE
+	li "𐑑𐑱𐑤 𐑢𐑦𐑐" ; TAIL WHIP
+	li "𐑐𐑶𐑟𐑩𐑯 𐑕𐑑𐑦𐑙" ; POISON STING
+	li "𐑑𐑢𐑦𐑯𐑰𐑛𐑩𐑤" ; TWINEEDLE
+	li "𐑐𐑦𐑯 𐑥𐑦𐑕𐑲𐑤" ; PIN MISSILE
+	li "𐑤𐑽" ; LEER
+	li "𐑚𐑲𐑑" ; BITE
+	li "𐑜𐑮𐑬𐑤" ; GROWL
+	li "𐑮𐑹" ; ROAR
+	li "𐑕𐑦𐑙" ; SING
+	li "𐑕𐑵𐑐𐑼𐑕𐑪𐑯𐑦𐑒" ; SUPERSONIC
+	li "𐑕𐑪𐑯𐑦𐑒 𐑚𐑵𐑥" ; SONICBOOM
+	li "𐑛𐑦𐑕𐑱𐑚𐑩𐑤" ; DISABLE
+	li "𐑨𐑕𐑦𐑛" ; ACID
+	li "𐑧𐑥𐑚𐑼" ; EMBER
+	li "𐑓𐑤𐑱𐑥𐑔𐑮𐑴𐑼" ; FLAMETHROWER
+	li "𐑥𐑦𐑕𐑑" ; MIST
+	li "𐑢𐑷𐑑𐑼 𐑜𐑳𐑯" ; WATER GUN
+	li "𐑣𐑲𐑛𐑮𐑴 𐑐𐑳𐑥𐑐" ; HYDRO PUMP
+	li "𐑕𐑻𐑓" ; SURF
+	li "𐑲𐑕 𐑚𐑰𐑥" ; ICE BEAM
+	li "𐑚𐑤𐑦𐑟𐑼𐑛" ; BLIZZARD
+	li "𐑕𐑲𐑚𐑰𐑥" ; PSYBEAM
+	li "𐑚𐑳𐑚𐑩𐑤 𐑚𐑰𐑥" ; BUBBLEBEAM
+	li "𐑹𐑹𐑩 𐑚𐑰𐑥" ; AURORA BEAM
+	li "𐑣𐑲𐑐𐑼 𐑚𐑰𐑥" ; HYPER BEAM
+	li "𐑐𐑧𐑒" ; PECK
+	li "𐑛𐑮𐑦𐑤 𐑐𐑧𐑒" ; DRILL PECK
+	li "𐑕𐑩𐑚𐑥𐑦𐑖𐑩𐑯" ; SUBMISSION
+	li "𐑤𐑴 𐑒𐑦𐑒" ; LOW KICK
+	li "𐑒𐑬𐑯𐑑𐑼" ; COUNTER
+	li "𐑕𐑲𐑟𐑥𐑦𐑒 𐑑𐑪𐑕" ; SEISMIC TOSS
+	li "𐑕𐑑𐑮𐑧𐑙𐑔" ; STRENGTH
+	li "𐑩𐑚𐑕𐑹𐑚" ; ABSORB
+	li "𐑥𐑧𐑜𐑩 𐑛𐑮𐑱𐑯" ; MEGA DRAIN
+	li "𐑤𐑰𐑗 𐑕𐑰𐑛" ; LEECH SEED
+	li "𐑜𐑮𐑴𐑔" ; GROWTH
+	li "𐑮𐑱𐑟𐑼 𐑤𐑰𐑓" ; RAZOR LEAF
+	li "𐑕𐑴𐑤𐑼 𐑚𐑰𐑥" ; SOLARBEAM
+	li "𐑐𐑶𐑟𐑩𐑯 𐑐𐑬𐑛𐑼" ; POISONPOWDER
+	li "𐑕𐑑𐑳𐑯 𐑕𐑐𐑹" ; STUN SPORE
+	li "𐑕𐑤𐑰𐑐 𐑐𐑬𐑛𐑼" ; SLEEP POWDER
+	li "𐑐𐑧𐑑𐑩𐑤 𐑛𐑭𐑯𐑕" ; PETAL DANCE
+	li "𐑕𐑑𐑮𐑦𐑙 𐑖𐑪𐑑" ; STRING SHOT
+	li "𐑛𐑮𐑨𐑜𐑩𐑯 𐑮𐑱𐑡" ; DRAGON RAGE
+	li "𐑓𐑲𐑼 𐑕𐑐𐑦𐑯" ; FIRE SPIN
+	li "𐑔𐑳𐑯𐑛𐑼 𐑖𐑪𐑒" ; THUNDERSHOCK
+	li "𐑔𐑳𐑯𐑛𐑼𐑚𐑴𐑤𐑑" ; THUNDERBOLT
+	li "𐑔𐑳𐑯𐑛𐑼 𐑢𐑱𐑝" ; THUNDER WAVE
+	li "𐑔𐑳𐑯𐑛𐑼" ; THUNDER
+	li "𐑮𐑪𐑒 𐑔𐑮𐑴" ; ROCK THROW
+	li "𐑻𐑔𐑒𐑢𐑱𐑒" ; EARTHQUAKE
+	li "𐑓𐑦𐑖𐑼" ; FISSURE
+	li "𐑛𐑦𐑜" ; DIG
+	li "𐑑𐑪𐑒𐑕𐑦𐑒" ; TOXIC
+	li "𐑒𐑩𐑯𐑓𐑿𐑠𐑩𐑯" ; CONFUSION
+	li "𐑕𐑲𐑒𐑦𐑒" ; PSYCHIC
+	li "𐑣𐑦𐑐𐑯𐑴𐑕𐑦𐑕" ; HYPNOSIS
+	li "𐑥𐑧𐑛𐑦𐑑𐑱𐑑" ; MEDITATE
+	li "𐑩𐑡𐑦𐑤𐑦𐑑𐑦" ; AGILITY
+	li "𐑒𐑢𐑦𐑒 𐑩𐑑𐑨𐑒" ; QUICK ATTACK
+	li "𐑮𐑱𐑡" ; RAGE
+	li "𐑑𐑧𐑤𐑦𐑐𐑹𐑑" ; TELEPORT
+	li "𐑯𐑲𐑑 𐑖𐑱𐑛" ; NIGHT SHADE
+	li "𐑥𐑦𐑥𐑦𐑒" ; MIMIC
+	li "𐑕𐑒𐑮𐑰𐑗" ; SCREECH
+	li "𐑛𐑳𐑚𐑩𐑤 𐑑𐑰𐑥" ; DOUBLE TEAM
+	li "𐑮𐑦𐑒𐑳𐑝𐑼" ; RECOVER
+	li "𐑣𐑸𐑛𐑩𐑯" ; HARDEN
+	li "𐑥𐑦𐑯𐑦𐑥𐑲𐑟" ; MINIMIZE
+	li "𐑕𐑥𐑴𐑒𐑕𐑒𐑮𐑰𐑯" ; SMOKESCREEN
+	li "𐑒𐑩𐑯𐑓𐑿𐑟 𐑮𐑱" ; CONFUSE RAY
+	li "𐑢𐑦𐑞𐑛𐑮𐑷" ; WITHDRAW
+	li "𐑛𐑦𐑓𐑧𐑯𐑕 𐑒𐑻𐑤" ; DEFENSE CURL
+	li "𐑚𐑨𐑮𐑽" ; BARRIER
+	li "𐑤𐑲𐑑 𐑕𐑒𐑮𐑰𐑯" ; LIGHT SCREEN
+	li "𐑣𐑱𐑟" ; HAZE
+	li "𐑮𐑦𐑓𐑤𐑧𐑒𐑑" ; REFLECT
+	li "𐑓𐑴𐑒𐑩𐑕 𐑧𐑯𐑼𐑡𐑦" ; FOCUS ENERGY
+	li "𐑚𐑲𐑛" ; BIDE
+	li "𐑥𐑧𐑑𐑮𐑩𐑯𐑴𐑥" ; METRONOME
+	li "𐑥𐑦𐑮𐑼 𐑥𐑵𐑝" ; MIRROR MOVE
+	li "𐑕𐑧𐑤𐑓𐑛𐑦𐑕𐑑𐑮𐑳𐑒𐑑" ; SELFDESTRUCT
+	li "𐑧𐑜 𐑚𐑪𐑥" ; EGG BOMB
+	li "𐑤𐑦𐑒" ; LICK
+	li "𐑕𐑥𐑪𐑜" ; SMOG
+	li "𐑕𐑤𐑳𐑡" ; SLUDGE
+	li "𐑚𐑴𐑯 𐑒𐑤𐑳𐑚" ; BONE CLUB
+	li "𐑓𐑲𐑼 𐑚𐑤𐑭𐑕𐑑" ; FIRE BLAST
+	li "𐑢𐑷𐑑𐑼𐑓𐑷𐑤" ; WATERFALL
+	li "𐑒𐑤𐑨𐑥𐑐" ; CLAMP
+	li "𐑕𐑢𐑦𐑓𐑑" ; SWIFT
+	li "𐑕𐑒𐑳𐑤 𐑚𐑨𐑖" ; SKULL BASH
+	li "𐑕𐑐𐑲𐑒 𐑒𐑨𐑯𐑩𐑯" ; SPIKE CANNON
+	li "𐑒𐑩𐑯𐑕𐑑𐑮𐑦𐑒𐑑" ; CONSTRICT
+	li "𐑨𐑥𐑯𐑰𐑟𐑾" ; AMNESIA
+	li "𐑒𐑩𐑯𐑰𐑕𐑦𐑕" ; KINESIS
+	li "𐑕𐑪𐑓𐑑 𐑚𐑶𐑤𐑛" ; SOFTBOILED
+	li "𐑣𐑲 𐑡𐑳𐑥𐑐 𐑒𐑦𐑒" ; HI JUMP KICK
+	li "𐑜𐑤𐑺" ; GLARE
+	li "𐑛𐑮𐑰𐑥 𐑰𐑑𐑼" ; DREAM EATER
+	li "𐑐𐑶𐑟𐑩𐑯 𐑜𐑨𐑕" ; POISON GAS
+	li "𐑚𐑨𐑮𐑭𐑠" ; BARRAGE
+	li "𐑤𐑰𐑗 𐑤𐑲𐑓" ; LEECH LIFE
+	li "𐑤𐑳𐑝𐑤𐑦 𐑒𐑦𐑕" ; LOVELY KISS
+	li "𐑕𐑒𐑲 𐑩𐑑𐑨𐑒" ; SKY ATTACK
+	li "𐑑𐑮𐑨𐑯𐑕𐑓𐑹𐑥" ; TRANSFORM
+	li "𐑚𐑳𐑚𐑩𐑤" ; BUBBLE
+	li "𐑛𐑦𐑟𐑦 𐑐𐑳𐑯𐑗" ; DIZZY PUNCH
+	li "𐑕𐑐𐑹" ; SPORE
+	li "𐑓𐑤𐑨𐑖" ; FLASH
+	li "𐑕𐑲𐑢𐑱𐑝" ; PSYWAVE
+	li "𐑕𐑐𐑤𐑨𐑖" ; SPLASH
+	li "𐑨𐑕𐑦𐑛 𐑸𐑥𐑼" ; ACID ARMOR
+	li "𐑒𐑮𐑨𐑚 𐑣𐑨𐑥𐑼" ; CRABHAMMER
+	li "𐑦𐑒𐑕𐑐𐑤𐑴𐑠𐑩𐑯" ; EXPLOSION
+	li "𐑓𐑘𐑫𐑼𐑦 𐑕𐑢𐑲𐑐𐑕" ; FURY SWIPES
+	li "𐑚𐑴𐑯𐑥𐑼𐑨𐑙" ; BONEMERANG
+	li "𐑮𐑧𐑕𐑑" ; REST
+	li "𐑮𐑪𐑒 𐑕𐑤𐑲𐑛" ; ROCK SLIDE
+	li "𐑣𐑲𐑐𐑼 𐑓𐑨𐑙" ; HYPER FANG
+	li "𐑖𐑸𐑐𐑩𐑯" ; SHARPEN
+	li "𐑒𐑩𐑯𐑝𐑻𐑠𐑩𐑯" ; CONVERSION
+	li "𐑑𐑮𐑲 𐑩𐑑𐑨𐑒" ; TRI ATTACK
+	li "𐑕𐑵𐑐𐑼 𐑓𐑨𐑙" ; SUPER FANG
+	li "𐑕𐑤𐑨𐑖" ; SLASH
+	li "𐑕𐑳𐑚𐑕𐑑𐑦𐑑𐑿𐑑" ; SUBSTITUTE
+	li "𐑕𐑑𐑮𐑳𐑜𐑩𐑤" ; STRUGGLE
 	assert_list_length NUM_ATTACKS
