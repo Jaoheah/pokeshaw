327dec4c31bac9c571ba413f32b4571a072c18ba
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 12:28:28 +0100

    Transliterate the Pewter Speech House

--- text/PewterSpeechHouse.asm
+++ text/PewterSpeechHouse.asm
@@ -1,19 +1,17 @@
 _PewterSpeechHouseGamblerText::
-	text "#MON learn new"
-	line "techniques as"
-	cont "they grow!"
+	text "#𐑥𐑪𐑯 𐑤𐑻𐑯 𐑯𐑿"
+	line "𐑑𐑧𐑒𐑯𐑰𐑒𐑕 𐑨𐑟 𐑞𐑱"
+	cont "𐑜𐑮𐑴!"
 
-	para "But, some moves"
-	line "must be taught by"
-	cont "the trainer!"
+	para "𐑚𐑳𐑑, 𐑕𐑳𐑥 𐑥𐑵𐑝𐑟 𐑥𐑳𐑕𐑑"
+	line "𐑚𐑰 𐑑𐑷𐑑 𐑚𐑲 𐑞 𐑑𐑮𐑱𐑯𐑼!"
 	done
 
 _PewterSpeechHouseYoungsterText::
-	text "#MON become"
-	line "easier to catch"
-	cont "when they are"
-	cont "hurt or asleep!"
+	text "#𐑥𐑪𐑯 𐑚𐑦𐑒𐑳𐑥 𐑰𐑟𐑦𐑼"
+	line "𐑑 𐑒𐑨𐑗 𐑢𐑧𐑯 𐑞𐑱 𐑸"
+	cont "𐑣𐑻𐑑 𐑹 𐑩𐑕𐑤𐑰𐑐!"
 
-	para "But, it's not a"
-	line "sure thing!"
+	para "𐑚𐑳𐑑, 𐑦𐑑𐑕 𐑯𐑪𐑑 𐑩 𐑖𐑫𐑼"
+	line "𐑔𐑦𐑙!"
 	done
