92fd62ac1b5c6787df8a2c91880638f72916d3b7
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 20:07:00 +0100

    Transliterate the trainer names

--- data/trainers/names.asm
+++ data/trainers/names.asm
@@ -1,50 +1,50 @@
 TrainerNames::
 	list_start
-	li "YOUNGSTER"
-	li "BUG CATCHER"
-	li "LASS"
-	li "SAILOR"
-	li "JR.TRAINER♂"
-	li "JR.TRAINER♀"
-	li "POKéMANIAC"
-	li "SUPER NERD"
-	li "HIKER"
-	li "BIKER"
-	li "BURGLAR"
-	li "ENGINEER"
-	li "JUGGLER"
-	li "FISHERMAN"
-	li "SWIMMER"
-	li "CUE BALL"
-	li "GAMBLER"
-	li "BEAUTY"
-	li "PSYCHIC"
-	li "ROCKER"
-	li "JUGGLER"
-	li "TAMER"
-	li "BIRD KEEPER"
-	li "BLACKBELT"
-	li "RIVAL1"
-	li "PROF.OAK"
-	li "CHIEF"
-	li "SCIENTIST"
-	li "GIOVANNI"
-	li "ROCKET"
-	li "COOLTRAINER♂"
-	li "COOLTRAINER♀"
-	li "BRUNO"
-	li "𐑚𐑮𐑪𐑒"
-	li "MISTY"
-	li "LT.SURGE"
-	li "ERIKA"
-	li "KOGA"
-	li "BLAINE"
-	li "SABRINA"
-	li "GENTLEMAN"
-	li "RIVAL2"
-	li "RIVAL3"
-	li "LORELEI"
-	li "CHANNELER"
-	li "AGATHA"
-	li "LANCE"
+	li "𐑘𐑳𐑙𐑕𐑑𐑼" ; YOUNGSTER
+	li "𐑚𐑳𐑜 𐑒𐑨𐑗𐑼" ; BUG CATCHER
+	li "𐑤𐑨𐑕" ; LASS
+	li "𐑕𐑱𐑤𐑼" ; SAILOR
+	li "𐑡𐑼.𐑑𐑮𐑱𐑯𐑼♂" ; JR.TRAINER♂
+	li "𐑡𐑼.𐑑𐑮𐑱𐑯𐑼♀" ; JR.TRAINER♀
+	li "𐑐𐑪𐑒𐑱𐑥𐑱𐑯𐑾𐑒" ; POKéMANIAC
+	li "𐑕𐑵𐑐𐑼 𐑯𐑻𐑛" ; SUPER NERD
+	li "𐑣𐑲𐑒𐑼" ; HIKER
+	li "𐑚𐑲𐑒𐑼" ; BIKER
+	li "𐑚𐑻𐑜𐑤𐑼" ; BURGLAR
+	li "𐑧𐑯𐑡𐑦𐑯𐑽" ; ENGINEER
+	li "𐑡𐑳𐑜𐑤𐑼" ; JUGGLER
+	li "𐑓𐑦𐑖𐑼𐑥𐑩𐑯" ; FISHERMAN
+	li "𐑕𐑢𐑦𐑥𐑼" ; SWIMMER
+	li "𐑒𐑿 𐑚𐑷𐑤" ; CUE BALL
+	li "𐑜𐑨𐑥𐑚𐑤𐑼" ; GAMBLER
+	li "𐑚𐑿𐑑𐑦" ; BEAUTY
+	li "𐑕𐑲𐑒𐑦𐑒" ; PSYCHIC
+	li "𐑮𐑪𐑒𐑼" ; ROCKER
+	li "𐑡𐑳𐑜𐑤𐑼" ; JUGGLER
+	li "𐑑𐑱𐑥𐑼" ; TAMER
+	li "𐑚𐑻𐑛 𐑒𐑰𐑐𐑼" ; BIRD KEEPER
+	li "𐑚𐑤𐑨𐑒𐑚𐑧𐑤𐑑" ; BLACKBELT
+	li "𐑮𐑲𐑝𐑩𐑤1" ; RIVAL1
+	li "𐑐𐑮𐑪𐑓.𐑴𐑒" ; PROF.OAK
+	li "𐑗𐑰𐑓" ; CHIEF
+	li "𐑕𐑲𐑩𐑯𐑑𐑦𐑕𐑑" ; SCIENTIST
+	li "𐑡𐑴𐑝𐑭𐑯𐑦" ; GIOVANNI
+	li "𐑮𐑪𐑒𐑩𐑑" ; ROCKET
+	li "𐑒𐑵𐑤 𐑑𐑮𐑱𐑯𐑼♂" ; COOLTRAINER♂
+	li "𐑒𐑵𐑤 𐑑𐑮𐑱𐑯𐑼♀" ; COOLTRAINER♀
+	li "𐑚𐑮𐑵𐑯𐑴" ; BRUNO
+	li "𐑚𐑮𐑪𐑒" ; 𐑚𐑮𐑪𐑒
+	li "𐑥𐑦𐑕𐑑𐑦" ; MISTY
+	li "𐑤𐑑.𐑕𐑻𐑡" ; LT.SURGE
+	li "𐑧𐑮𐑦𐑒𐑩" ; ERIKA
+	li "𐑒𐑴𐑜𐑩" ; KOGA
+	li "𐑚𐑤𐑱𐑯" ; BLAINE
+	li "𐑕𐑩𐑚𐑮𐑰𐑯𐑩" ; SABRINA
+	li "𐑡𐑧𐑯𐑑𐑩𐑤𐑥𐑩𐑯" ; GENTLEMAN
+	li "𐑮𐑲𐑝𐑩𐑤2" ; RIVAL2
+	li "𐑮𐑲𐑝𐑩𐑤3" ; RIVAL3
+	li "𐑤𐑹𐑩𐑤𐑱" ; LORELEI
+	li "𐑗𐑨𐑯𐑩𐑤𐑼" ; CHANNELER
+	li "𐑨𐑜𐑩𐑔𐑩" ; AGATHA
+	li "𐑤𐑭𐑯𐑕" ; LANCE
 	assert_list_length NUM_TRAINERS
