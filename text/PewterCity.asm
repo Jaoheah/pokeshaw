1966b49c4ffd28fcf126f45826c9fe66ace8cbb1
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-06 12:10:59 +0100

    Transliterate Pewter City

--- text/PewterCity.asm
+++ text/PewterCity.asm
@@ -1,117 +1,110 @@
 _PewterCityCooltrainerFText::
-	text "It's rumored that"
-	line "CLEFAIRYs came"
-	cont "from the moon!"
+	text "𐑦𐑑𐑕 𐑮𐑵𐑥𐑼𐑛 𐑞𐑨𐑑"
+	line "𐑒𐑤𐑩𐑓𐑺𐑦𐑟 𐑒𐑱𐑥"
+	cont "𐑓𐑮𐑪𐑥 𐑞 𐑥𐑵𐑯!"
 
-	para "They appeared "
-	line "after MOON STONE"
-	cont "fell on MT.MOON."
+	para "𐑞𐑱 𐑩𐑐𐑽𐑛 𐑭𐑓𐑑𐑼 ·𐑥𐑵𐑯"
+	line "𐑕𐑑𐑴𐑯 𐑓𐑧𐑤 𐑪𐑯"
+	cont "·𐑥𐑑.𐑥𐑵𐑯."
 	done
 
 _PewterCityCooltrainerMText::
-	text "There aren't many"
-	line "serious #MON"
-	cont "trainers here!"
+	text "𐑞𐑺 𐑸𐑯𐑑 𐑥𐑧𐑯𐑦 𐑕𐑽𐑾𐑕"
+	line "#𐑥𐑪𐑯 𐑑𐑮𐑱𐑯𐑼𐑟"
+	cont "𐑣𐑽!"
 
-	para "They're all like"
-	line "BUG CATCHERs,"
-	cont "but PEWTER GYM's"
-	cont "BROCK is totally"
-	cont "into it!"
+	para "𐑞𐑱𐑼 𐑷𐑤 𐑤𐑲𐑒 𐑚𐑳𐑜"
+	line "𐑒𐑨𐑗𐑼𐑟, 𐑚𐑳𐑑 ·𐑐𐑿𐑑𐑼"
+	cont "𐑡𐑦𐑥𐑟 ·𐑚𐑮𐑪𐑒 𐑦𐑟"
+	cont "𐑑𐑴𐑑𐑩𐑤𐑦 𐑦𐑯𐑑𐑵 𐑦𐑑!"
 	done
 
 _PewterCitySuperNerd1DidYouCheckOutMuseumText::
-	text "Did you check out"
-	line "the MUSEUM?"
+	text "𐑛𐑦𐑛 𐑿 𐑗𐑧𐑒 𐑬𐑑 𐑞"
+	line "𐑥𐑿𐑟𐑾𐑥?"
 	done
 
 _PewterCitySuperNerd1WerentThoseFossilsAmazingText::
-	text "Weren't those"
-	line "fossils from MT."
-	cont "MOON amazing?"
+	text "𐑢𐑻𐑯𐑑 𐑞𐑴𐑟 𐑓𐑪𐑕𐑩𐑤𐑟"
+	line "𐑓𐑮𐑪𐑥 ·𐑥𐑑.𐑥𐑵𐑯"
+	cont "𐑩𐑥𐑱𐑟𐑦𐑙?"
 	done
 
 _PewterCitySuperNerd1YouHaveToGoText::
-	text "Really?"
-	line "You absolutely"
-	cont "have to go!"
+	text "𐑮𐑾𐑤𐑦?"
+	line "𐑿 𐑨𐑚𐑕𐑩𐑤𐑵𐑑𐑤𐑦 𐑣𐑨𐑓 𐑑"
+	cont "𐑜𐑴!"
 	done
 
 _PewterCitySuperNerd1ItsRightHereText::
-	text "It's right here!"
-	line "You have to pay"
-	cont "to get in, but"
-	cont "it's worth it!"
-	cont "See you around!"
+	text "𐑦𐑑𐑕 𐑮𐑲𐑑 𐑣𐑽! 𐑿 𐑣𐑨𐑓 𐑑"
+	line "𐑐𐑱 𐑑 𐑜𐑧𐑑 𐑦𐑯, 𐑚𐑳𐑑"
+	cont "𐑦𐑑𐑕 𐑢𐑻𐑔 𐑦𐑑! 𐑕𐑰 𐑿"
+	cont "𐑼𐑬𐑯𐑛!"
 	done
 
 _PewterCitySuperNerd2DoYouKnowWhatImDoingText::
-	text "Psssst!"
-	line "Do you know what"
-	cont "I'm doing?"
+	text "𐑐𐑕𐑕𐑕𐑕𐑑!"
+	line "𐑛𐑵 𐑿 𐑯𐑴 𐑢𐑪𐑑 𐑲𐑥"
+	cont "𐑛𐑵𐑦𐑙?"
 	done
 
 _PewterCitySuperNerd2ThatsRightText::
-	text "That's right!"
-	line "It's hard work!"
+	text "𐑞𐑨𐑑𐑕 𐑮𐑲𐑑! 𐑦𐑑𐑕 𐑣𐑸𐑛"
+	line "𐑢𐑻𐑒!"
 	done
 
 _PewterCitySuperNerd2ImSprayingRepelText::
-	text "I'm spraying REPEL"
-	line "to keep #MON"
-	cont "out of my garden!"
+	text "𐑲𐑥 𐑕𐑐𐑮𐑱𐑦𐑙 «𐑮𐑦𐑐𐑧𐑤» 𐑑"
+	line "𐑒𐑰𐑐 #𐑥𐑪𐑯 𐑬𐑑 𐑝"
+	cont "𐑥𐑲 𐑜𐑸𐑛𐑩𐑯!"
 	done
 
 _PewterCityYoungsterYoureATrainerFollowMeText::
-	text "You're a trainer"
-	line "right? BROCK's"
-	cont "looking for new"
-	cont "challengers!"
-	cont "Follow me!"
+	text "𐑿𐑼 𐑩 𐑑𐑮𐑱𐑯𐑼 𐑮𐑲𐑑?"
+	line "·𐑚𐑮𐑪𐑒𐑕 𐑤𐑫𐑒𐑦𐑙 𐑓 𐑯𐑿"
+	cont "𐑗𐑨𐑤𐑩𐑯𐑡𐑼𐑟! 𐑓𐑪𐑤𐑴 𐑥𐑰!"
 	done
 
 _PewterCityYoungsterGoTakeOnBrockText::
-	text "If you have the"
-	line "right stuff, go"
-	cont "take on BROCK!"
+	text "𐑦𐑓 𐑿 𐑣𐑨𐑝 𐑞 𐑮𐑲𐑑"
+	line "𐑕𐑑𐑳𐑓, 𐑜𐑴 𐑑𐑱𐑒 𐑪𐑯"
+	cont "·𐑚𐑮𐑪𐑒!"
 	done
 
 _PewterCityTrainerTipsText::
-	text "TRAINER TIPS"
+	text "𐑑𐑮𐑱𐑯𐑼 𐑑𐑦𐑐𐑕"
 
-	para "Any #MON that"
-	line "takes part in"
-	cont "battle, however"
-	cont "short, earns EXP!"
+	para "𐑧𐑯𐑦 #𐑥𐑪𐑯 𐑞𐑨𐑑"
+	line "𐑑𐑱𐑒𐑕 𐑐𐑸𐑑 𐑦𐑯 𐑚𐑨𐑑𐑩𐑤,"
+	cont "𐑣𐑬𐑧𐑝𐑼 𐑖𐑹𐑑, 𐑻𐑯𐑟"
+	cont "𐑧𐑒𐑕𐑐𐑰!"
 	done
 
 _PewterCityPoliceNoticeSignText::
-	text "NOTICE!"
+	text "·𐑯𐑴𐑑𐑦𐑕!"
 
-	para "Thieves have been"
-	line "stealing #MON"
-	cont "fossils at MT."
-	cont "MOON! Please call"
-	cont "PEWTER POLICE"
-	cont "with any info!"
+	para "𐑔𐑰𐑝𐑟 𐑣𐑨𐑝 𐑚𐑰𐑯 𐑕𐑑𐑰𐑤𐑦𐑙"
+	line "#𐑥𐑪𐑯 𐑓𐑪𐑕𐑩𐑤𐑟 𐑨𐑑"
+	cont "·𐑥𐑑.𐑥𐑵𐑯! 𐑐𐑤𐑰𐑟 𐑒𐑷𐑤"
+	cont "·𐑐𐑿𐑑𐑼 𐑐𐑩𐑤𐑰𐑕 𐑢𐑦𐑞"
+	cont "𐑧𐑯𐑦 𐑦𐑯𐑓𐑴!"
 	done
 
 _PewterCityMuseumSignText::
-	text "PEWTER MUSEUM"
-	line "OF SCIENCE"
+	text "𐑐𐑿𐑑𐑼 𐑥𐑿𐑟𐑾𐑥 𐑝 𐑕𐑲𐑩𐑯𐑕"
 	done
 
 _PewterCityGymSignText::
-	text "PEWTER CITY"
-	line "#MON GYM"
-	cont "LEADER: BROCK"
+	text "𐑐𐑿𐑑𐑼 𐑕𐑦𐑑𐑦"
+	line "#𐑥𐑪𐑯 𐑡𐑦𐑥"
+	cont "𐑤𐑰𐑛𐑼: ·𐑚𐑮𐑪𐑒"
 
-	para "The Rock Solid"
-	line "#MON Trainer!"
+	para "𐑞 𐑮𐑪𐑒 𐑕𐑪𐑤𐑦𐑛"
+	line "#𐑥𐑪𐑯 𐑑𐑮𐑱𐑯𐑼!"
 	done
 
 _PewterCitySignText::
-	text "PEWTER CITY"
-	line "A Stone Gray"
-	cont "City"
+	text "·𐑐𐑿𐑑𐑼 𐑕𐑦𐑑𐑦"
+	line "𐑩 𐑕𐑑𐑴𐑯 𐑜𐑮𐑱 𐑕𐑦𐑑𐑦"
 	done
