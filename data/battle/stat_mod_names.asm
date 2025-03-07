e84d8a20f833a6d1bccf0fa83b6eaa702dde63b4
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-07 10:13:00 +0100

    Transliterate stat names

--- data/battle/stat_mod_names.asm
+++ data/battle/stat_mod_names.asm
@@ -3,13 +3,13 @@
 
 StatModTextStrings:
 	list_start
-	li "ATTACK"
-	li "DEFENSE"
-	li "SPEED"
-	li "SPECIAL"
+	li "𐑩𐑑𐑨𐑒" ; ATTACK
+	li "𐑛𐑦𐑓𐑧𐑯𐑕" ; DEFENSE
+	li "𐑕𐑐𐑰𐑛" ; SPEED
+	li "𐑕𐑐𐑧𐑖𐑩𐑤" ; SPECIAL
 	assert_list_length SPECIAL_DOWN_SIDE_EFFECT - ATTACK_DOWN_SIDE_EFFECT + 1
-	li "ACCURACY"
-	li "EVADE"
+	li "𐑨𐑒𐑘𐑼𐑩𐑕𐑦y" ; ACCURACY
+	li "𐑦𐑝𐑱𐑛" ; EVADE
 	assert_list_length NUM_STAT_MODS - 2 ; two bytes are unused
 	assert_list_length EVASION_UP1_EFFECT - ATTACK_UP1_EFFECT + 1
 	assert_list_length EVASION_DOWN1_EFFECT - ATTACK_DOWN1_EFFECT + 1
--- data/battle/stat_names.asm
+++ data/battle/stat_names.asm
@@ -2,9 +2,9 @@
 
 VitaminStats:
 	list_start
-	li "HEALTH"
-	li "ATTACK"
-	li "DEFENSE"
-	li "SPEED"
-	li "SPECIAL"
+	li "𐑣𐑧𐑤𐑔" ; HEALTH
+	li "𐑩𐑑𐑨𐑒" ; ATTACK
+	li "𐑛𐑦𐑓𐑧𐑯𐑕" ; DEFENSE
+	li "𐑕𐑐𐑰𐑛" ; SPEED
+	li "𐑕𐑐𐑧𐑖𐑩𐑤" ; SPECIAL
 	assert_list_length NUM_STATS
