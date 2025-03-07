924314a72b692352a737fd7598af2e2a70997740
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-07 10:39:22 +0100

    Transliterate item names

--- data/items/names.asm
+++ data/items/names.asm
@@ -1,101 +1,101 @@
 ItemNames::
 	list_start
-	li "MASTER BALL"
-	li "ULTRA BALL"
-	li "GREAT BALL"
-	li "POKé BALL"
-	li "TOWN MAP"
-	li "BICYCLE"
+	li "𐑥𐑭𐑕𐑑𐑼 𐑚𐑷𐑤" ; MASTER BALL
+	li "𐑳𐑤𐑑𐑮𐑩 𐑚𐑷𐑤" ; ULTRA BALL
+	li "𐑜𐑮𐑱𐑑 𐑚𐑷𐑤" ; GREAT BALL
+	li "𐑐𐑴𐑒𐑱 𐑚𐑷𐑤" ; POKé BALL
+	li "𐑑𐑬𐑯 𐑥𐑨𐑐" ; TOWN MAP
+	li "𐑚𐑲𐑕𐑦𐑒𐑩𐑤" ; BICYCLE
 	li "?????" ; SURFBOARD
-	li "SAFARI BALL"
-	li "POKéDEX"
-	li "MOON STONE"
-	li "ANTIDOTE"
-	li "BURN HEAL"
-	li "ICE HEAL"
-	li "AWAKENING"
-	li "PARLYZ HEAL"
-	li "FULL RESTORE"
-	li "MAX POTION"
-	li "HYPER POTION"
-	li "SUPER POTION"
-	li "POTION"
-	li "BOULDERBADGE"
-	li "CASCADEBADGE"
-	li "THUNDERBADGE"
-	li "RAINBOWBADGE"
-	li "SOULBADGE"
-	li "MARSHBADGE"
-	li "VOLCANOBADGE"
-	li "EARTHBADGE"
-	li "ESCAPE ROPE"
-	li "REPEL"
-	li "OLD AMBER"
-	li "FIRE STONE"
-	li "THUNDERSTONE"
-	li "WATER STONE"
-	li "HP UP"
-	li "PROTEIN"
-	li "IRON"
-	li "CARBOS"
-	li "CALCIUM"
-	li "RARE CANDY"
-	li "DOME FOSSIL"
-	li "HELIX FOSSIL"
-	li "SECRET KEY"
+	li "𐑕𐑩𐑓𐑸𐑦 𐑚𐑷𐑤" ; SAFARI BALL
+	li "𐑐𐑴𐑒𐑱𐑛𐑧𐑒𐑕" ; POKéDEX
+	li "𐑥𐑵𐑯 𐑕𐑑𐑴𐑯" ; MOON STONE
+	li "𐑨𐑯𐑑𐑦𐑛𐑴𐑑" ; ANTIDOTE
+	li "𐑚𐑻𐑯 𐑣𐑰𐑤" ; BURN HEAL
+	li "𐑲𐑕 𐑣𐑰𐑤" ; ICE HEAL
+	li "𐑩𐑢𐑱𐑒𐑩𐑯𐑦𐑙" ; AWAKENING
+	li "𐑐𐑨𐑮𐑩𐑤𐑲𐑟 𐑣𐑰𐑤" ; PARLYZ HEAL
+	li "𐑓𐑫𐑤 𐑮𐑦𐑕𐑑𐑹" ; FULL RESTORE
+	li "𐑥𐑨𐑒𐑕 𐑐𐑴𐑖𐑩𐑯" ; MAX POTION
+	li "𐑣𐑲𐑐𐑼 𐑐𐑴𐑖𐑩𐑯" ; HYPER POTION
+	li "𐑕𐑵𐑐𐑼 𐑐𐑴𐑖𐑩𐑯" ; SUPER POTION
+	li "𐑐𐑴𐑖𐑩𐑯" ; POTION
+	li "𐑚𐑴𐑤𐑛𐑼 𐑚𐑨𐑡" ; BOULDERBADGE
+	li "𐑒𐑨𐑕𐑒𐑱𐑛 𐑚𐑨𐑡" ; CASCADEBADGE
+	li "𐑔𐑳𐑯𐑛𐑼 𐑚𐑨𐑡" ; THUNDERBADGE
+	li "𐑮𐑱𐑯𐑚𐑴 𐑚𐑨𐑡" ; RAINBOWBADGE
+	li "𐑕𐑴𐑤 𐑚𐑨𐑡" ; SOULBADGE
+	li "𐑥𐑸𐑖 𐑚𐑨𐑡" ; MARSHBADGE
+	li "𐑝𐑪𐑤𐑒𐑱𐑯𐑴 𐑚𐑨𐑡" ; VOLCANOBADGE
+	li "𐑻𐑔 𐑚𐑨𐑡" ; EARTHBADGE
+	li "𐑦𐑕𐑒𐑱𐑐 𐑮𐑴𐑐" ; ESCAPE ROPE
+	li "𐑮𐑦𐑐𐑧𐑤" ; REPEL
+	li "𐑴𐑤𐑛 𐑨𐑥𐑚𐑼" ; OLD AMBER
+	li "𐑓𐑲𐑼 𐑕𐑑𐑴𐑯" ; FIRE STONE
+	li "𐑔𐑳𐑯𐑛𐑼 𐑕𐑑𐑴𐑯" ; THUNDERSTONE
+	li "𐑢𐑷𐑑𐑼 𐑕𐑑𐑴𐑯" ; WATER STONE
+	li "⸰𐑣𐑐 𐑳𐑐" ; HP UP
+	li "𐑐𐑮𐑴𐑑𐑰𐑯" ; PROTEIN
+	li "𐑲𐑼𐑯" ; IRON
+	li "𐑒𐑸𐑚𐑴𐑟" ; CARBOS
+	li "𐑒𐑨𐑤𐑕𐑾𐑥" ; CALCIUM
+	li "𐑮𐑺 𐑒𐑨𐑯𐑛𐑦" ; RARE CANDY
+	li "𐑛𐑴𐑥 𐑓𐑪𐑕𐑩𐑤" ; DOME FOSSIL
+	li "𐑣𐑰𐑤𐑦𐑒𐑕 𐑓𐑪𐑕𐑩𐑤" ; HELIX FOSSIL
+	li "𐑕𐑰𐑒𐑮𐑩𐑑 𐑒𐑰" ; SECRET KEY
 	li "?????" ; ITEM_2C
-	li "BIKE VOUCHER"
-	li "X ACCURACY"
-	li "LEAF STONE"
-	li "CARD KEY"
-	li "NUGGET"
-	li "PP UP" ; ITEM_32
-	li "POKé DOLL"
-	li "FULL HEAL"
-	li "REVIVE"
-	li "MAX REVIVE"
-	li "GUARD SPEC."
-	li "SUPER REPEL"
-	li "MAX REPEL"
-	li "DIRE HIT"
-	li "COIN"
-	li "FRESH WATER"
-	li "SODA POP"
-	li "LEMONADE"
-	li "S.S.TICKET"
-	li "GOLD TEETH"
-	li "X ATTACK"
-	li "X DEFEND"
-	li "X SPEED"
-	li "X SPECIAL"
-	li "COIN CASE"
-	li "OAK's PARCEL"
-	li "ITEMFINDER"
-	li "SILPH SCOPE"
-	li "POKé FLUTE"
-	li "LIFT KEY"
-	li "EXP.ALL"
-	li "OLD ROD"
-	li "GOOD ROD"
-	li "SUPER ROD"
-	li "PP UP"
-	li "ETHER"
-	li "MAX ETHER"
-	li "ELIXER"
-	li "MAX ELIXER"
+	li "𐑚𐑲𐑒 𐑝𐑬𐑗𐑼" ; BIKE VOUCHER
+	li "𐑧𐑒𐑕 𐑨𐑒𐑘𐑼𐑩𐑕𐑦" ; X ACCURACY
+	li "𐑤𐑰𐑓 𐑕𐑑𐑴𐑯" ; LEAF STONE
+	li "𐑒𐑸𐑛 𐑒𐑰" ; CARD KEY
+	li "𐑯𐑳𐑜𐑩𐑑" ; NUGGET
+	li "⸰𐑐𐑐 𐑳𐑐" ; ITEM_32
+	li "𐑐𐑴𐑒𐑱 𐑛𐑪𐑤" ; POKé DOLL
+	li "𐑓𐑫𐑤 𐑣𐑰𐑤" ; FULL HEAL
+	li "𐑮𐑦𐑝𐑲𐑝" ; REVIVE
+	li "𐑥𐑨𐑒𐑕 𐑮𐑦𐑝𐑲𐑝" ; MAX REVIVE
+	li "𐑜𐑸𐑛 𐑕𐑐𐑧𐑒." ; GUARD SPEC.
+	li "𐑕𐑵𐑐𐑼 𐑮𐑦𐑐𐑧𐑤" ; SUPER REPEL
+	li "𐑥𐑨𐑒𐑕 𐑮𐑦𐑐𐑧𐑤" ; MAX REPEL
+	li "𐑛𐑲𐑼 𐑣𐑦𐑑" ; DIRE HIT
+	li "𐑒𐑶𐑯" ; COIN
+	li "𐑓𐑮𐑧𐑖 𐑢𐑷𐑑𐑼" ; FRESH WATER
+	li "𐑕𐑴𐑛𐑩 𐑐𐑪𐑐" ; SODA POP
+	li "𐑤𐑧𐑥𐑩𐑯𐑱𐑛" ; LEMONADE
+	li "⸰𐑕𐑕 𐑑𐑦𐑒𐑩𐑑" ; S.S.TICKET
+	li "𐑜𐑴𐑤𐑛 𐑑𐑰𐑔" ; GOLD TEETH
+	li "𐑧𐑒𐑕 𐑩𐑑𐑨𐑒" ; X ATTACK
+	li "𐑧𐑒𐑕 𐑛𐑦𐑓𐑧𐑯𐑛" ; X DEFEND
+	li "𐑧𐑒𐑕 𐑕𐑐𐑰𐑛" ; X SPEED
+	li "𐑧𐑒𐑕 𐑕𐑐𐑧𐑖𐑩𐑤" ; X SPECIAL
+	li "𐑒𐑶𐑯 𐑒𐑱𐑕" ; COIN CASE
+	li "𐑴𐑒𐑕 𐑐𐑸𐑕𐑩𐑤" ; OAK's PARCEL
+	li "𐑲𐑑𐑩𐑥 𐑓𐑲𐑯𐑛𐑼" ; ITEMFINDER
+	li "𐑕𐑦𐑤𐑓 𐑕𐑒𐑴𐑐" ; SILPH SCOPE
+	li "𐑐𐑴𐑒𐑱 𐑓𐑤𐑵𐑑" ; POKé FLUTE
+	li "𐑤𐑦𐑓𐑑 𐑒𐑰" ; LIFT KEY
+	li "𐑧𐑒𐑕𐑐𐑰 𐑷𐑤" ; EXP.ALL
+	li "𐑴𐑤𐑛 𐑮𐑪𐑛" ; OLD ROD
+	li "𐑜𐑫𐑛 𐑮𐑪𐑛" ; GOOD ROD
+	li "𐑕𐑵𐑐𐑼 𐑮𐑪𐑛" ; SUPER ROD
+	li "⸰𐑐𐑐 𐑳𐑐" ; PP UP
+	li "𐑰𐑔𐑼" ; ETHER
+	li "𐑥𐑨𐑒𐑕 𐑰𐑔𐑼" ; MAX ETHER
+	li "𐑦𐑤𐑦𐑒𐑕𐑼" ; ELIXER
+	li "𐑥𐑨𐑒𐑕 𐑦𐑤𐑦𐑒𐑕𐑼" ; MAX ELIXER
 	assert_list_length NUM_ITEMS
-	li "B2F"
-	li "B1F"
-	li "1F"
-	li "2F"
-	li "3F"
-	li "4F"
-	li "5F"
-	li "6F"
-	li "7F"
-	li "8F"
-	li "9F"
-	li "10F"
-	li "11F"
-	li "B4F"
+	li "𐑚2𐑓"
+	li "𐑚1𐑓"
+	li "1𐑓"
+	li "2𐑓"
+	li "3𐑓"
+	li "4𐑓"
+	li "5𐑓"
+	li "6𐑓"
+	li "7𐑓"
+	li "8𐑓"
+	li "9𐑓"
+	li "10𐑓"
+	li "11𐑓"
+	li "𐑚4𐑓"
 	assert_list_length NUM_ITEMS + NUM_FLOORS
