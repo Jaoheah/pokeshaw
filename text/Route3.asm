2cdf87ea3540e1ff06f92f177886d4da6f8c01f6
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-07 12:18:24 +0100

    Transliterate route 3 text

--- text/Route3.asm
+++ text/Route3.asm
@@ -1,145 +1,139 @@
 _Route3Text1::
-	text "Whew... I better"
-	line "take a rest..."
-	cont "Groan..."
+	text "𐑓𐑿... 𐑲 𐑚𐑧𐑑𐑼 𐑑𐑱𐑒 𐑩"
+	line "𐑮𐑧𐑕𐑑... 𐑜𐑮𐑴𐑯..."
 
-	para "That tunnel from"
-	line "CERULEAN takes a"
-	cont "lot out of you!"
+	para "𐑞𐑨𐑑 𐑑𐑳𐑯𐑩𐑤 𐑓𐑮𐑪𐑥"
+	line "·𐑕𐑦𐑮𐑵𐑤𐑾𐑯 𐑑𐑱𐑒𐑕 𐑩"
+	cont "𐑤𐑪𐑑 𐑬𐑑 𐑝 𐑿!"
 	done
 
 _Route3Youngster1BattleText::
-	text "Hey! I met you in"
-	line "VIRIDIAN FOREST!"
+	text "𐑣𐑱! 𐑲 𐑥𐑧𐑑 𐑿 𐑦𐑯"
+	line "·𐑝𐑦𐑮𐑦𐑛𐑾𐑯 𐑓𐑪𐑮𐑦𐑕𐑑!"
 	done
 
 _Route3Youngster1EndBattleText::
-	text "You"
-	line "beat me again!"
+	text "𐑿"
+	line "𐑚𐑰𐑑 𐑥𐑰 𐑩𐑜𐑧𐑯!"
 	prompt
 
 _Route3Youngster1AfterBattleText::
-	text "There are other"
-	line "kinds of #MON"
-	cont "than those found"
-	cont "in the forest!"
+	text "𐑞𐑺 𐑸 𐑳𐑞𐑼 𐑒𐑲𐑯𐑛𐑟 𐑝"
+	line "#𐑥𐑪𐑯 𐑞𐑨𐑯 𐑞𐑴𐑟"
+	cont "𐑓𐑬𐑯𐑛 𐑦𐑯 𐑞 𐑓𐑪𐑮𐑦𐑕𐑑!"
 	done
 
 _Route3Youngster2BattleText::
-	text "Hi! I like shorts!"
-	line "They're comfy and"
-	cont "easy to wear!"
+	text "𐑣𐑲! 𐑲 𐑤𐑲𐑒 𐑖𐑹𐑑𐑕!"
+	line "𐑞𐑱𐑼 𐑒𐑳𐑥𐑓𐑦 𐑯 𐑰𐑟𐑦 𐑑"
+	cont "𐑢𐑺!"
 	done
 
 _Route3Youngster2EndBattleText::
-	text "I don't"
-	line "believe it!"
+	text "𐑲 𐑛𐑴𐑯𐑑"
+	line "𐑚𐑦𐑤𐑰𐑝 𐑦𐑑!"
 	prompt
 
 _Route3Youngster2AfterBattleText::
-	text "Are you storing"
-	line "your #MON on"
-	cont "PC? Each BOX can"
-	cont "hold 20 #MON!"
+	text "𐑸 𐑿 𐑕𐑑𐑹𐑦𐑙 𐑘𐑹"
+	line "#𐑥𐑪𐑯 𐑪𐑯 𐑐𐑰𐑕𐑰?"
+	cont "𐑰𐑗 𐑚𐑪𐑒𐑕 𐑒𐑨𐑯 𐑣𐑴𐑤𐑛"
+	cont "20 #𐑥𐑪𐑯!"
 	done
 
 _Route3CooltrainerF1BattleText::
-	text "You looked at me,"
-	line "didn't you?"
+	text "𐑿 𐑤𐑫𐑒𐑑 𐑨𐑑 𐑥𐑰,"
+	line "𐑛𐑦𐑛𐑩𐑯𐑑 𐑿?"
 	done
 
 _Route3CooltrainerF1EndBattleText::
-	text "You're"
-	line "mean!"
+	text "𐑿𐑼"
+	line "𐑥𐑰𐑯!"
 	prompt
 
 _Route3CooltrainerF1AfterBattleText::
-	text "Quit staring if"
-	line "you don't want to"
-	cont "fight!"
+	text "𐑒𐑢𐑦𐑑 𐑕𐑑𐑺𐑦𐑙 𐑦𐑓 𐑿 𐑛𐑴𐑯𐑑"
+	line "𐑢𐑪𐑯𐑑 𐑑 𐑓𐑲𐑑!"
 	done
 
 _Route3Youngster3BattleText::
-	text "Are you a trainer?"
-	line "Let's fight!"
+	text "𐑸 𐑿 𐑩 𐑑𐑮𐑱𐑯𐑼? 𐑤𐑧𐑑𐑕"
+	line "𐑓𐑲𐑑!"
 	done
 
 _Route3Youngster3EndBattleText::
-	text "If I"
-	line "had new #MON I"
-	cont "would've won!"
+	text "𐑦𐑓 𐑲"
+	line "𐑣𐑨𐑛 𐑯𐑿 #𐑥𐑪𐑯 𐑲"
+	cont "𐑢𐑫𐑛𐑩𐑝 𐑢𐑳𐑯!"
 	prompt
 
 _Route3Youngster3AfterBattleText::
-	text "If a #MON BOX"
-	line "on the PC gets"
-	cont "full, just switch"
-	cont "to another BOX!"
+	text "𐑦𐑓 𐑩 #𐑥𐑪𐑯 𐑚𐑪𐑒𐑕"
+	line "𐑪𐑯 𐑞 𐑐𐑰𐑕𐑰 𐑜𐑧𐑑𐑕"
+	cont "𐑓𐑫𐑤, 𐑡𐑳𐑕𐑑 𐑕𐑢𐑦𐑗 𐑑"
+	cont "𐑩𐑯𐑳𐑞𐑼 𐑚𐑪𐑒𐑕!"
 	done
 
 _Route3CooltrainerF2BattleText::
-	text "That look you"
-	line "gave me, it's so"
-	cont "intriguing!"
+	text "𐑞𐑨𐑑 𐑤𐑫𐑒 𐑿 𐑜𐑱𐑝 𐑥𐑰,"
+	line "𐑦𐑑𐑕 𐑕𐑴 𐑦𐑯𐑑𐑮𐑰𐑜𐑦𐑙!"
 	done
 
 _Route3CooltrainerF2EndBattleText::
-	text "Be nice!"
+	text "𐑚𐑰 𐑯𐑲𐑕!"
 	prompt
 
 _Route3CooltrainerF2AfterBattleText::
-	text "Avoid fights by"
-	line "not letting"
-	cont "people see you!"
+	text "𐑩𐑝𐑶𐑛 𐑓𐑲𐑑𐑕 𐑚𐑲 𐑯𐑪𐑑"
+	line "𐑤𐑧𐑑𐑦𐑙 𐑐𐑰𐑐𐑩𐑤 𐑕𐑰 𐑿!"
 	done
 
 _Route3Youngster4BattleText::
-	text "Hey! You're not"
-	line "wearing shorts!"
+	text "𐑣𐑱! 𐑿𐑼 𐑯𐑪𐑑 𐑢𐑺𐑦𐑙"
+	line "𐑖𐑹𐑑𐑕!"
 	done
 
 _Route3Youngster4EndBattleText::
-	text "Lost!"
-	line "Lost! Lost!"
+	text "𐑤𐑪𐑕𐑑!"
+	line "𐑤𐑪𐑕𐑑! 𐑤𐑪𐑕𐑑!"
 	prompt
 
 _Route3Youngster4AfterBattleText::
-	text "I always wear"
-	line "shorts, even in"
-	cont "winter!"
+	text "𐑲 𐑷𐑤𐑢𐑱𐑟 𐑢𐑺 𐑖𐑹𐑑𐑕,"
+	line "𐑰𐑝𐑩𐑯 𐑦𐑯 𐑢𐑦𐑯𐑑𐑼!"
 	done
 
 _Route3Youngster5BattleText::
-	text "You can fight my"
-	line "new #MON!"
+	text "𐑿 𐑒𐑨𐑯 𐑓𐑲𐑑 𐑥𐑲 𐑯𐑿"
+	line "#𐑥𐑪𐑯!"
 	done
 
 _Route3Youngster5EndBattleText::
-	text "Done"
-	line "like dinner!"
+	text "𐑛𐑳𐑯"
+	line "𐑤𐑲𐑒 𐑛𐑦𐑯𐑼!"
 	prompt
 
 _Route3Youngster5AfterBattleText::
-	text "Trained #MON"
-	line "are stronger than"
-	cont "the wild ones!"
+	text "𐑑𐑮𐑱𐑯𐑛 #𐑥𐑪𐑯 𐑸"
+	line "𐑕𐑑𐑮𐑪𐑙𐑜𐑼 𐑞𐑨𐑯 𐑞"
+	cont "𐑢𐑲𐑤𐑛 𐑢𐑳𐑯𐑟!"
 	done
 
 _Route3CooltrainerF3BattleText::
-	text "Eek! Did you"
-	line "touch me?"
+	text "𐑰𐑒! 𐑛𐑦𐑛 𐑿 𐑑𐑳𐑗"
+	line "𐑥𐑰?"
 	done
 
 _Route3CooltrainerF3EndBattleText::
-	text "That's it?"
+	text "𐑞𐑨𐑑𐑕 𐑦𐑑?"
 	prompt
 
 _Route3CooltrainerF3AfterBattleText::
-	text "ROUTE 4 is at the"
-	line "foot of MT.MOON."
+	text "·𐑮𐑵𐑑 4 𐑦𐑟 𐑨𐑑 𐑞 𐑓𐑫𐑑"
+	line "𐑝 ·𐑥𐑑.𐑥𐑵𐑯."
 	done
 
 _Route3SignText::
-	text "ROUTE 3"
-	line "MT.MOON AHEAD"
+	text "·𐑮𐑵𐑑 3"
+	line "·𐑥𐑑.𐑥𐑵𐑯 𐑩𐑣𐑧𐑛"
 	done
