1386544e65e00eb53e8a1b2b5a64e23e105f2da0
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-07 12:04:38 +0100

    Fix stray y in the transliteration

--- data/battle/stat_mod_names.asm
+++ data/battle/stat_mod_names.asm
@@ -8,7 +8,7 @@ StatModTextStrings:
 	li "𐑕𐑐𐑰𐑛" ; SPEED
 	li "𐑕𐑐𐑧𐑖𐑩𐑤" ; SPECIAL
 	assert_list_length SPECIAL_DOWN_SIDE_EFFECT - ATTACK_DOWN_SIDE_EFFECT + 1
-	li "𐑨𐑒𐑘𐑼𐑩𐑕𐑦y" ; ACCURACY
+	li "𐑨𐑒𐑘𐑼𐑩𐑕𐑦" ; ACCURACY
 	li "𐑦𐑝𐑱𐑛" ; EVADE
 	assert_list_length NUM_STAT_MODS - 2 ; two bytes are unused
 	assert_list_length EVASION_UP1_EFFECT - ATTACK_UP1_EFFECT + 1
