972c897a70f8c9b95b63c1030113fdff40b0803b
Neil Roberts <bpeeluk@yahoo.co.uk>
2025-03-07 19:29:52 +0100

    Transliterate route 4

--- text/Route4.asm
+++ text/Route4.asm
@@ -1,35 +1,34 @@
 _Route4CooltrainerF1Text::
-	text "Ouch! I tripped"
-	line "over a rocky"
-	cont "#MON, GEODUDE!"
+	text "𐑬𐑗! 𐑲 𐑑𐑮𐑦𐑐𐑑 𐑴𐑝𐑼 𐑩"
+	line "𐑮𐑪𐑒𐑦 #𐑥𐑪𐑯,"
+	cont "𐑡𐑰𐑴𐑛𐑵𐑛!"
 	done
 
 _Route4CooltrainerF2BattleText::
-	text "I came to get my"
-	line "mushroom #MON!"
+	text "𐑲 𐑒𐑱𐑥 𐑑 𐑜𐑧𐑑 𐑥𐑲"
+	line "𐑥𐑳𐑖𐑮𐑵𐑥 #𐑥𐑪𐑯!"
 	done
 
 _Route4CooltrainerF2EndBattleText::
-	text "Oh! My cute"
-	line "mushroom #MON!"
+	text "𐑴! 𐑥𐑲 𐑒𐑿𐑑"
+	line "𐑥𐑳𐑖𐑮𐑵𐑥 #𐑥𐑪𐑯!"
 	prompt
 
 _Route4CooltrainerF2AfterBattleText::
-	text "There might not"
-	line "be any more"
-	cont "mushrooms here."
+	text "𐑞𐑺 𐑥𐑲𐑑 𐑯𐑪𐑑 𐑚𐑰 𐑧𐑯𐑦"
+	line "𐑥𐑹 𐑥𐑳𐑖𐑮𐑵𐑥𐑟 𐑣𐑽."
 
-	para "I think I got"
-	line "them all."
+	para "𐑲 𐑔𐑦𐑙𐑒 𐑲 𐑜𐑪𐑑 𐑞𐑧𐑥"
+	line "𐑷𐑤."
 	done
 
 _Route4MtMoonSignText::
-	text "MT.MOON"
-	line "Tunnel Entrance"
+	text "·𐑥𐑑.𐑥𐑵𐑯"
+	line "𐑑𐑳𐑯𐑩𐑤 𐑧𐑯𐑑𐑮𐑩𐑯𐑕"
 	done
 
 _Route4SignText::
-	text "ROUTE 4"
-	line "MT.MOON -"
-	cont "CERULEAN CITY"
+	text "·𐑮𐑵𐑑 4"
+	line "·𐑥𐑑.𐑥𐑵𐑯 -"
+	cont "·𐑕𐑦𐑮𐑵𐑤𐑾𐑯 𐑕𐑦𐑑𐑦"
 	done
